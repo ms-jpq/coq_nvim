3b46e945a9cddec2fa72997e6a6155ff6a23ed9d
Fly me to the moon <git@bigly.dog>
2020-07-15 21:34:32 -0700

    ooda loop

--- rplugin/python3/fast_comp/__init__.py
+++ rplugin/python3/fast_comp/__init__.py
@@ -1,4 +1,4 @@
-from asyncio import AbstractEventLoop, run_coroutine_threadsafe
+from asyncio import AbstractEventLoop, Queue, run_coroutine_threadsafe
 from concurrent.futures import ThreadPoolExecutor
 from traceback import format_exc
 from typing import Any, Awaitable, Sequence
@@ -6,8 +6,8 @@ from typing import Any, Awaitable, Sequence
 from pynvim import Nvim, command, function, plugin
 
 from .completion import merge
-from .da import anext
-from .nvim import autocmd, call, complete
+from .nvim import autocmd, call, complete, print
+from .scheduler import schedule
 from .settings import initial, load_factories
 
 
@@ -16,6 +16,7 @@ class Main:
     def __init__(self, nvim: Nvim):
         self.nvim = nvim
         self.chan = ThreadPoolExecutor(max_workers=1)
+        self.ch: Queue = Queue(1)
 
         self.settings = initial(user_config={})
         factories = load_factories(settings=self.settings)
@@ -48,19 +49,29 @@ class Main:
                 self.nvim, events=("TextChangedP",), fn="_FCtextchangedp",
             )
 
+        async def forever() -> None:
+            while True:
+                try:
+                    await self._ooda()
+                except Exception as e:
+                    await print(self.nvim, e, error=True)
+
         if self._initialized:
             return
         else:
             self._initialized = True
             self._submit(setup())
 
-    async def populate_pum(self) -> None:
-        results = await anext(self.gen)
-        await call(self.nvim, complete(self.nvim, comp=results))
+    async def _ooda(self) -> None:
+        async for comp in schedule(chan=self.ch, gen=self.gen):
+            await call(self.nvim, complete(self.nvim, comp=comp))
 
     @function("_FCtextchangedi")
     def text_changed_i(self, args: Sequence[Any]) -> None:
-        self._submit(self.populate_pum())
+        async def put() -> None:
+            await self.ch.put(None)
+
+        self._submit(put())
 
     @function("_FCtextchangedp")
     def text_changed_p(self, args: Sequence[Any]) -> None:
---
+++ rplugin/python3/fast_comp/scheduler.py
@@ -0,0 +1,13 @@
+from asyncio import FIRST_COMPLETED, Queue, sleep, wait
+from typing import AsyncIterator, TypeVar
+
+from .da import anext
+
+T = TypeVar("T")
+
+
+async def schedule(chan: Queue, gen: AsyncIterator[T]) -> AsyncIterator[T]:
+
+    while True:
+        t = await anext(gen)
+        yield t
