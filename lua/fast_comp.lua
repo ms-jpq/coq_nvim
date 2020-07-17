3e16e591160dfdcc5e5b5f188e3c29b1b15282fe
Fly me to the moon <git@bigly.dog>
2020-07-17 14:55:06 -0700

    remove schedulke

--- lua/fast_comp.lua
+++ lua/fast_comp.lua
@@ -16,9 +16,7 @@ local list_comp_candidates = function (request_id)
     local pos = lsp.util.make_position_params()
     _, cancel = lsp.buf_request(0, "textDocument/completion", pos, function (err, _, rows)
       assert(not err, err)
-      vim.schedule(function ()
-        fn._FCnotify("lsp", request_id, rows)
-      end)
+      fn._FCnotify("lsp", request_id, rows)
     end)
   end
 end
