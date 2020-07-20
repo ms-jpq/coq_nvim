a79b79362bcaef5c3dfbc608c7e54bb864cf8003
Fly me to the moon <git@bigly.dog>
2020-07-20 10:52:14 -0700

    idx

--- rplugin/python3/fast_comp/completion.py
+++ rplugin/python3/fast_comp/completion.py
@@ -165,7 +165,7 @@ async def merge(
             )
             steps: Sequence[Step] = tuple((c for co in comps for c in co))
             push(feed, steps)
-            completions = chain(steps, cached)
+            completions = chain(steps)
             return position, fuzzy(completions)
         else:
             return position, iter(())
--- rplugin/python3/fast_comp/patch.py
+++ rplugin/python3/fast_comp/patch.py
@@ -32,25 +32,28 @@ def replace_lines(nvim: Nvim, payload: Payload) -> None:
             )
         )
         + col
+        + 1
     )
-    pre = old[: idx - len(new_prefix) - 1]
-    post = old[idx + len(new_suffix) + 1 :]
+
+    pre = old[: idx - len(old_prefix)]
+    post = old[idx + len(old_suffix) + 1 :]
     before = pre + new_prefix
     after = new_suffix + post
     new_lines = (before + after).splitlines()
 
-    new_row = btm_idx + len(before) - 1
-    new_col = len(before[-1]) - 1
+    r_idx = before.rfind(linesep)
+    new_row = btm_idx + before.count(linesep) + 1
+    new_col = len(before) - (0 if r_idx == -1 else r_idx)
 
     nvim.api.buf_set_lines(buf, btm_idx, top_idx, True, new_lines)
     win = nvim.api.get_current_win()
-    nvim.api.win_set_cursor(win, (row, new_col))
+    nvim.api.win_set_cursor(win, (new_row, new_col))
 
-    msg = f"{[old_lines]} - {before},{after}"
+    msg = f"{row}, {btm_idx}, {new_row}"
     nvim.api.out_write(msg + "\n")
 
-    msg = f"{old_prefix} -> {new_prefix}"
-    nvim.api.out_write(msg + "\n")
+
+
 
 def patch(nvim: Nvim, comp: Dict[str, Any]) -> None:
     data = comp.get("user_data")
