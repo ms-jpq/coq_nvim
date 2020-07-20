f7c3a0b134cc72e7b2550a6f53b6cabc924d5dfa
Fly me to the moon <git@bigly.dog>
2020-07-20 14:44:11 -0700

    success loading modules

--- rplugin/python3/fast_comp/da.py
+++ rplugin/python3/fast_comp/da.py
@@ -1,6 +1,7 @@
 from importlib.util import module_from_spec, spec_from_file_location
 from json import load
 from os.path import basename, splitext
+from sys import modules
 from types import ModuleType
 from typing import Any, Optional, TypeVar, cast
 
@@ -38,6 +39,7 @@ def load_module(path: str) -> ModuleType:
     name, _ = splitext(basename(path))
     spec = spec_from_file_location(name, path, submodule_search_locations=[])
     mod = module_from_spec(spec)
+    modules[mod.__name__] = mod
     cast(Any, spec.loader).exec_module(mod)
     return mod
 
