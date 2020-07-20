ba97a04d0e215f28fe67f808f26fb927c706455f
Fly me to the moon <git@bigly.dog>
2020-07-20 02:30:45 -0700

    debug

--- rplugin/python3/fast_comp/patch.py
+++ rplugin/python3/fast_comp/patch.py
@@ -1,5 +1,6 @@
 from os import linesep
-from typing import Any, Dict, Sequence, Tuple, cast
+from itertools import takewhile
+from typing import Any, Dict, Sequence, cast
 
 from pynvim import Nvim
 
@@ -14,45 +15,42 @@ def replace_lines(nvim: Nvim, payload: Payload) -> None:
     old_suffix = payload.old_suffix
     new_suffix = payload.new_suffix
 
-    btm_idx = row - old_prefix.count(linesep)
-    top_idx = row + old_suffix.count(linesep) + 1
+    old_lc = old_prefix.count(linesep)
+    new_lc = old_suffix.count(linesep)
+    btm_idx = row - old_lc
+    top_idx = row + new_lc + 1
 
     buf = nvim.api.get_current_buf()
     old_lines: Sequence[str] = nvim.api.buf_get_lines(buf, btm_idx, top_idx, True)
 
-    def seek() -> int:
-        i = 0
-        for r, line in enumerate(old_lines, btm_idx):
-            for c, _ in enumerate(line, 0):
-                if r == row and c == col:
-                    return i
-                else:
-                    i += 1
-        return i
-
-    idx = seek()
     old = "".join(old_lines)
-    pre = old[: idx - len(new_prefix)]
+    idx = (
+        sum(
+            map(
+                lambda t: t[1],
+                takewhile(lambda t: t[0] < old_lc, enumerate(map(len, old_lines))),
+            )
+        )
+        + col
+    )
+    pre = old[: idx - len(new_prefix) - 1]
     post = old[idx + len(new_suffix) + 1 :]
-    new_lines = (pre + new_prefix + new_suffix + post).splitlines()
+    before = pre + new_prefix
+    after = new_suffix + post
+    new_lines = (before + after).splitlines()
 
-    def seek2() -> Tuple[int, int]:
-        cutoff = len(pre) + len(new_prefix)
-        i = 0
-        for r, line in enumerate(new_lines, btm_idx):
-            for c, _ in enumerate(line, 0):
-                if i == cutoff:
-                    return r, c
-                else:
-                    i += 1
-        return -1, -1
-
-    new_row, new_col = seek2()
+    new_row = btm_idx + len(before) - 1
+    new_col = len(before[-1]) - 1
 
     nvim.api.buf_set_lines(buf, btm_idx, top_idx, True, new_lines)
     win = nvim.api.get_current_win()
-    nvim.api.win_set_cursor(win, (new_row, new_col))
+    nvim.api.win_set_cursor(win, (row, new_col))
 
+    msg = f"{[old_lines]} - {before},{after}"
+    nvim.api.out_write(msg + "\n")
+
+    msg = f"{old_prefix} -> {new_prefix}"
+    nvim.api.out_write(msg + "\n")
 
 def patch(nvim: Nvim, comp: Dict[str, Any]) -> None:
     data = comp.get("user_data")
