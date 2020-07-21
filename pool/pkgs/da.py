536ac41b3b8625fb12eb41a0aee074453388b042
Fly me to the moon <git@bigly.dog>
2020-07-20 18:51:56 -0700

    parse subsequences

--- pool/pkgs/da.py
+++ pool/pkgs/da.py
@@ -1,6 +1,16 @@
-from typing import AsyncIterator, Optional, TypeVar
+from typing import AsyncIterator, Iterator, Optional, Sequence, TypeVar
 
 T = TypeVar("T")
+
+
+def subsequences(seq: Sequence[T], reverse: bool = False) -> Iterator[T]:
+    if not reverse:
+        for i in range(1, len(seq)):
+            yield seq[:i]
+    if reverse:
+        for i in range(len(seq) - 1, 0, -1):
+            yield seq[i:]
+    yield seq
 
 
 async def anext(aiter: AsyncIterator[T], default: Optional[T] = None) -> Optional[T]:
