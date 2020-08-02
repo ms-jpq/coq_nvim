d345c50bfa1eef210415f6b43b79f393443d95d0
Fly me to the moon <git@bigly.dog>
2020-08-02 15:11:19 -0700

    async sql wrapper

---
+++ rplugin/python3/nap/clients/pkgs/sql.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from asyncio import get_running_loop
+from concurrent.futures import ThreadPoolExecutor
+from sqlite3 import Connection, Cursor, Row, connect
+from typing import Any, Callable, Iterable, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+class AsyncExecutor:
+    def __init__(self) -> None:
+        self.loop = get_running_loop()
+        self.chan = ThreadPoolExecutor(max_workers=1)
+
+    async def run(self, cont: Callable[[], T]) -> T:
+        return await self.loop.run_in_executor(self.chan, cont)
+
+    def submit(self, cont: Callable[[], T]) -> T:
+        return self.chan.submit(cont).result()
+
+
+class CURSOR:
+    def __init__(self, chan: AsyncExecutor, cursor: Cursor) -> None:
+        self.chan = chan
+        self.cursor = cursor
+
+    async def __aenter__(self) -> CURSOR:
+        return self
+
+    async def __aexit__(self, *_: Any) -> None:
+        await self.chan.run(self.cursor.close)
+
+    def __aiter__(self) -> CURSOR:
+        return self
+
+    async def __anext__(self) -> Row:
+        row = await self.fetch_one()
+        if row is None:
+            raise StopAsyncIteration
+        else:
+            return row
+
+    async def fetch_one(self) -> Row:
+        return await self.chan.run(self.cursor.fetchone)
+
+
+class CONN:
+    def __init__(
+        self, database: str = ":memory:", isolation_level: Optional[str] = None
+    ) -> None:
+        self.chan = AsyncExecutor()
+
+        def cont() -> Connection:
+            return connect(database, isolation_level=isolation_level)
+
+        self.conn = self.chan.submit(cont)
+
+    async def __aenter__(self) -> CONN:
+        return self
+
+    async def __aexit__(self, *_: Any) -> None:
+        await self.chan.run(self.conn.close)
+
+    async def execute(self, sql: str, params: Iterable[Any] = ()) -> CURSOR:
+        def cont() -> CURSOR:
+            cursor = self.conn.execute(sql, params)
+            return CURSOR(chan=self.chan, cursor=cursor)
+
+        return await self.chan.run(cont)
