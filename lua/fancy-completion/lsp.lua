159480324006d526e7361d590213b2d9dec79c58
Fly me to the moon <git@bigly.dog>
2020-08-01 18:33:56 -0700

    request id

--- lua/fancy-completion/lsp.lua
+++ lua/fancy-completion/lsp.lua
@@ -7,26 +7,31 @@ local req_id = nil
 
 local linesep = "\n"
 
+
+local notify = function (request_id, ret)
+  if request_id == req_id then
+    fn._FCnotify("lsp", request_id, ret)
+  end
+end
+
+
 local list_comp_candidates = function (request_id, row, col)
   req_id = request_id
   cancel()
   cancel = function () end
 
   if #lsp.buf_get_clients() == 0 then
-    fn._FCnotify("lsp", request_id, nil)
+    notify(request_id, nil)
   else
     local position = {line = row, character = col}
     local text_doc = lsp.util.make_text_document_params()
     local params = {position = position, textDocument=text_doc}
 
-    _, cancel = lsp.buf_request(0, "textDocument/completion", params, function (err, _, ans)
+    _, cancel = lsp.buf_request(0, "textDocument/completion", params, function (err, _, ret)
       if err then
         api.nvim_out_writeln("lsp error: " .. err .. linesep)
       end
-      if request_id == req_id then
-        local ret = ans or nil
-        fn._FCnotify("lsp", request_id, ret)
-      end
+      notify(request_id, ret)
     end)
   end
 end
