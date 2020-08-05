e35884f4625c0936fb4b6ae4634c1ea7272759b7
Fly me to the moon <git@bigly.dog>
2020-08-05 00:29:48 -0700

    remove sql

--- rplugin/python3/nap/shared/consts.py
+++ rplugin/python3/nap/shared/consts.py
@@ -4,7 +4,6 @@ __nap__ = dirname(dirname(realpath(__file__)))
 __base__ = dirname(dirname(dirname(__nap__)))
 __config__ = join(__base__, "config")
 __artifacts__ = join(__base__, "artifacts")
-__sql__ = join(__nap__, "clients", "sql")
 
 settings_json = join(__config__, "config.json")
 
