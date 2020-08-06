f46708d4864f97ff086b9a1a06d413d5e8d6617f
Fly me to the moon <git@bigly.dog>
2020-08-06 14:58:08 -0700

    rm

--- rplugin/python3/nap/clients/paths.py
+++ rplugin/python3/nap/clients/paths.py
@@ -6,25 +6,27 @@ from pathlib import Path
 
 from pynvim import Nvim
 
-from ..shared.da import anext
 from ..shared.types import Completion, Context, Seed, Source
 
 NAME = "paths"
 
 
-def parse_path(root: str, parent: str = "") -> Iterator[str]:
-    l, s, r = root.rpartition(sep)
-    if s == sep:
-        curr = f"{s}{r}{parent}"
-        yield from parse_path(l, parent=curr)
-        if sep not in l:
-            if l.endswith(".."):
-                yield ".." + curr
-            elif l.endswith("."):
-                yield "." + curr
-            elif l.endswith("~"):
-                yield str(Path.home()) + curr
-        yield curr
+def parse_path(root: str, parent: str = "") -> str:
+    def cont() -> Iterator[str]:
+        l, s, r = root.rpartition(sep)
+        if s == sep:
+            curr = f"{s}{r}{parent}"
+            yield from parse_path(l, parent=curr)
+            if sep not in l:
+                if l.endswith(".."):
+                    yield ".." + curr
+                elif l.endswith("."):
+                    yield "." + curr
+                elif l.endswith("~"):
+                    yield str(Path.home()) + curr
+            yield curr
+
+    return "".join(cont())
 
 
 def list_dir(path: str) -> Sequence[str]:
@@ -62,8 +64,6 @@ async def find_children(path: str, context: Context) -> Sequence[Completion]:
                     old_suffix=old_suffix,
                     new_suffix="",
                 )
-        else:
-            return
 
     def co() -> Sequence[Completion]:
         return tuple(cont())
@@ -74,14 +74,8 @@ async def find_children(path: str, context: Context) -> Sequence[Completion]:
 async def main(nvim: Nvim, chan: Queue, seed: Seed) -> Source:
     async def source(context: Context) -> AsyncIterator[Completion]:
         before = context.line_before
-
-        async def next_children() -> AsyncIterator[Sequence[Completion]]:
-            for path in parse_path(before):
-                children = await find_children(path, context=context)
-                yield children
-
-        co = await anext(next_children())
-        for c in co or ():
-            yield c
+        path = parse_path(before)
+        for child in await find_children(path, context):
+            yield child
 
     return source
