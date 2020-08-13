458db112cb4a826c5b6461cdaa0496d69d86bb41
Fly me to the moon <git@bigly.dog>
2020-08-13 02:40:20 -0700

    escape char

--- rplugin/python3/narc/clients/pkgs/sql.py
+++ rplugin/python3/narc/clients/pkgs/sql.py
@@ -3,7 +3,7 @@ from typing import AsyncIterator, Iterable, Iterator, Tuple
 from ...shared.sql import SQL_TYPES, AConnection, sql_escape
 
 ESCAPE_CHAR = "-"
-MATCH_ESCAPE = {"-"}
+MATCH_ESCAPE = {} | {ESCAPE_CHAR}
 
 
 _INIT = """
