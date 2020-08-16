e4e30e80fc3e0a33eff8e4d08f9762f420d12e63
Fly me to the moon <git@bigly.dog>
2020-08-15 21:51:21 -0700

    exe

--- rplugin/python3/narc/shared/sql.py
+++ rplugin/python3/narc/shared/sql.py
@@ -64,63 +64,63 @@ class ACursor(AbstractAsyncContextManager, AsyncIterator):
 
 class AConnection(AbstractAsyncContextManager):
     def __init__(self, database: str = ":memory:") -> None:
+        self.chan = Executor()
         self.lock = Lock()
-        self._chan = Executor()
 
         def cont() -> Connection:
             return connect(database)
 
-        self._conn = self._chan.run_sync(connect, database).result()
+        self._conn = self.chan.run_sync(connect, database).result()
 
     async def __aexit__(self, *_: Any) -> None:
-        await self._chan.run(self._conn.close)
+        await self.chan.run(self._conn.close)
 
     async def cursor(self) -> ACursor:
         def cont() -> ACursor:
             cursor = self._conn.cursor()
-            return ACursor(self._chan, cursor=cursor)
+            return ACursor(self.chan, cursor=cursor)
 
-        return await self._chan.run(cont)
+        return await self.chan.run(cont)
 
     async def iter_dump(self) -> AsyncIterator:
         def co() -> Iterator[str]:
             return self._conn.iterdump()
 
-        it = await self._chan.run(co)
+        it = await self.chan.run(co)
 
         def cont() -> Optional[str]:
             return next(it, None)
 
-        line = await self._chan.run(cont)
+        line = await self.chan.run(cont)
         while line is not None:
             yield line
-            line = await self._chan.run(cont)
+            line = await self.chan.run(cont)
 
     async def execute_script(self, script: str) -> ACursor:
         def cont() -> ACursor:
             cursor = self._conn.executescript(script)
-            return ACursor(self._chan, cursor=cursor)
+            return ACursor(self.chan, cursor=cursor)
 
-        return await self._chan.run(cont)
+        return await self.chan.run(cont)
 
     async def commit(self) -> None:
-        return await self._chan.run(self._conn.commit)
+        return await self.chan.run(self._conn.commit)
 
     async def execute(self, sql: str, params: Iterable[SQL_TYPES] = ()) -> ACursor:
         def cont() -> ACursor:
             cursor = self._conn.execute(sql, params)
-            return ACursor(self._chan, cursor=cursor)
+            return ACursor(self.chan, cursor=cursor)
 
-        return await self._chan.run(cont)
+        return await self.chan.run(cont)
 
     async def execute_many(
         self, sql: str, params: Iterable[Iterable[SQL_TYPES]] = ()
     ) -> ACursor:
         def cont() -> ACursor:
             cursor = self._conn.executemany(sql, params)
-            return ACursor(self._chan, cursor=cursor)
+            return ACursor(self.chan, cursor=cursor)
 
-        return await self._chan.run(cont)
+        return await self.chan.run(cont)
 
 
 def sql_escape(param: str, nono: Set[str], escape: str) -> str:
