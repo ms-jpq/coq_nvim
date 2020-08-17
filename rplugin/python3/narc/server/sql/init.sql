7b745a9749417425c7e5e7117f7b4962af415523
Fly me to the moon <git@bigly.dog>
2020-08-17 09:34:24 -0700

    fix relationships

--- rplugin/python3/narc/server/sql/init.sql
+++ rplugin/python3/narc/server/sql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS batches (
   p_row INTEGER NOT NULL,
-  p_col INTEGER NOT NULL,
+  p_col INTEGER NOT NULL
 );
 
 
@@ -10,34 +10,9 @@ CREATE TABLE IF NOT EXISTS sources (
 );
 
 
-CREATE TABLE IF NOT EXISTS medits (
-  old_prefix TEXT NOT NULL,
-  new_prefix TEXT NOT NULL,
-  old_suffix TEXT NOT NULL,
-  new_suffix TEXT NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS ledits (
-  begin_row INTEGER NOT NULL,
-  begin_col INTEGER NOT NULL,
-  end_row   INTEGER NOT NULL,
-  end_col   INTEGER NOT NULL,
-  text      INTEGER NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS snippets (
-  kind    TEXT NOT NULL,
-  content TEXT NOT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS suggestions (
   batch_id         INTEGER NOT NULL,
   source_id        INTEGER NOT NULL,
-  medit_id         INTEGER,
-  snippet_id       INTEGER,
   match            TEXT    NOT NULL,
   match_normalized TEXT    NOT NULL,
   label            TEXT,
@@ -45,17 +20,42 @@ CREATE TABLE IF NOT EXISTS suggestions (
   kind             TEXT,
   doc              TEXT    NOT NULL,
   ensure_unique    INTEGER NOT NULL,
-  match_syms       INTEGER NOT NULL
-  FOREIGN KEY (batch_id)   REFERENCES batches  (rowid),
-  FOREIGN KEY (source_id)  REFERENCES sources  (rowid),
-  FOREIGN KEY (medit_id)   REFERENCES medits   (rowid)
-  FOREIGN KEY (snippet_id) REFERENCES snippets (rowid)
+  match_syms       INTEGER NOT NULL,
+  use_cache        INTEGER NOT NULL,
+  FOREIGN KEY (batch_id)   REFERENCES batches  (rowid) ON DELETE CASCADE,
+  FOREIGN KEY (source_id)  REFERENCES sources  (rowid) ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS suggestion_ledits (
+CREATE TABLE IF NOT EXISTS medits (
   suggestions_id INTEGER NOT NULL,
-  ledit_id       INTEGER NOT NULL,
-  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid),
-  FOREIGN KEY (ledit_id)       REFERENCES ledits      (rowid)
+  old_prefix     TEXT    NOT NULL,
+  new_prefix     TEXT    NOT NULL,
+  old_suffix     TEXT    NOT NULL,
+  new_suffix     TEXT    NOT NULL,
+  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS ledits (
+  suggestions_id INTEGER NOT NULL,
+  begin_row      INTEGER NOT NULL,
+  begin_col      INTEGER NOT NULL,
+  end_row        INTEGER NOT NULL,
+  end_col        INTEGER NOT NULL,
+  text           INTEGER NOT NULL,
+  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS snippets (
+  suggestions_id INTEGER NOT NULL,
+  kind           TEXT    NOT NULL,
+  content        TEXT    NOT NULL,
+  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+);
+
+
+CREATE VIEW IF NOT EXISTS suggestions_view (
+
 );
