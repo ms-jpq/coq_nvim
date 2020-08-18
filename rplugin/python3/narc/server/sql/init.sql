023c348dbdc5efc8a84e29e1996cbbae83748257
Fly me to the moon <git@bigly.dog>
2020-08-18 11:27:53 -0700

    fk contraint

--- rplugin/python3/narc/server/sql/init.sql
+++ rplugin/python3/narc/server/sql/init.sql
@@ -34,46 +34,42 @@ CREATE INDEX sources_use_cache ON sources (use_cache);
 
 
 CREATE TABLE suggestions (
-  batch_id         INTEGER NOT NULL,
-  source_id        INTEGER NOT NULL,
+  rowid            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  batch_id         INTEGER NOT NULL REFERENCES batches (rowid) ON DELETE CASCADE,
+  source_id        INTEGER NOT NULL REFERENCES sources (rowid) ON DELETE CASCADE,
   match            TEXT    NOT NULL,
   match_normalized TEXT    NOT NULL,
   label            TEXT,
   sortby           TEXT,
   kind             TEXT,
-  doc              TEXT,
-  FOREIGN KEY (batch_id)  REFERENCES batches (rowid) ON DELETE CASCADE,
-  FOREIGN KEY (source_id) REFERENCES sources (rowid) ON DELETE CASCADE
+  doc              TEXT
 );
 CREATE INDEX suggestions_match_normalized ON suggestions (match_normalized);
 
 
 CREATE TABLE medits (
-  suggestions_id INTEGER NOT NULL,
+  suggestions_id INTEGER NOT NULL REFERENCES suggestions (rowid) ON DELETE CASCADE,
   old_prefix     TEXT    NOT NULL,
   new_prefix     TEXT    NOT NULL,
   old_suffix     TEXT    NOT NULL,
-  new_suffix     TEXT    NOT NULL,
-  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+  new_suffix     TEXT    NOT NULL
 );
 
 
 CREATE TABLE ledits (
-  suggestions_id INTEGER NOT NULL,
+  suggestions_id INTEGER NOT NULL REFERENCES suggestions (rowid) ON DELETE CASCADE,
   begin_row      INTEGER NOT NULL,
   begin_col      INTEGER NOT NULL,
   end_row        INTEGER NOT NULL,
   end_col        INTEGER NOT NULL,
-  text           INTEGER NOT NULL,
-  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+  text           INTEGER NOT NULL
 );
 
 
 CREATE TABLE snippets (
-  suggestions_id INTEGER NOT NULL,
+  suggestions_id INTEGER NOT NULL REFERENCES suggestions (rowid) ON DELETE CASCADE,
   kind           TEXT    NOT NULL,
-  content        TEXT    NOT NULL,
-  FOREIGN KEY (suggestions_id) REFERENCES suggestions (rowid) ON DELETE CASCADE
+  content        TEXT    NOT NULL
 );
 
 END;
