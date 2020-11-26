63951b4edd79923e75775f64da5459f47ca6de48
Fly me to the moon <git@bigly.dog>
2020-11-25 20:48:04 -0800

    set

--- rplugin/python3/kok/server/surround/sql.py
+++ rplugin/python3/kok/server/surround/sql.py
@@ -1,12 +1,14 @@
 from os.path import join, realpath
+from ...shared.sql import AConnection
 
 __base__ = join(realpath(__file__), "sql")
 
 _INIT = join(__base__, "init.sql")
 _PRAGMA = join(__base__, "pragma.sql")
-_POPULATE = join(__base__, "populate.sql")
-
-from ...shared.sql import AConnection
+_POPULATE_COUNT = join(__base__, "populate_counts.sql")
+_POPULATE_FILENAME = join(__base__, "populate_filenames.sql")
+_POPULATE_LOCATION = join(__base__, "populate_locations.sql")
+_QUERY = join(__base__, "query.sql")
 
 
 class DB:
