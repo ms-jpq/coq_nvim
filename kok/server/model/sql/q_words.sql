059f73cf9c9872ce4dd5c7e11598569443c6ed70
Fly me to the moon <git@bigly.dog>
2021-01-22 17:40:21 +0700

    esc

--- kok/server/model/sql/q_words.sql
+++ kok/server/model/sql/q_words.sql
@@ -3,6 +3,6 @@ SELECT
 FROM
   words
 WHERE
-  nword LIKE (REPLACE(SUBSTR(:q_nword, 1, :match_len), '!', '!!') || '%') ESCAPE '!'
-  AND
+  nword LIKE (REPLACE(REPLACE(REPLACE(SUBSTR(:q_nword, 1, :match_len), '!', '!!'), '%', '!%'), '_', '!_') || '%') ESCAPE '!'
+  AND 
   NOT :q_word LIKE _test ESCAPE '!'
