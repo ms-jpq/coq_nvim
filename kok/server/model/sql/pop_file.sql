70127667d4631e8a1400bc51d69838396c410b3b
Fly me to the moon <git@bigly.dog>
2021-01-22 15:53:32 +0700

    e

--- kok/server/model/sql/pop_file.sql
+++ kok/server/model/sql/pop_file.sql
@@ -1,4 +1,4 @@
-INSERT OR REPLACE INTO files (filename, filetype_id)
+REPLACE INTO files (filename, filetype_id)
 SELECT
   ?           AS filename,
   filetype_id AS filetype_id
--- kok/server/model/sql/pop_filetype.sql
+++ kok/server/model/sql/pop_filetype.sql
@@ -1,2 +1,2 @@
-INSERT OR REPLACE INTO filetypes (filetype)
+REPLACE INTO filetypes (filetype)
 VALUES (?)
