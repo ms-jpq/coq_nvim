1c62fee87b2de9e4a57e103a163c2fefc8d62be7
Fly me to the moon <git@bigly.dog>
2021-01-27 21:30:17 -0800

    setup py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages(exclude=("tests*",))
+package_data = {pkg: ("py.typed",) for pkg in packages}
+install_requires = Path("requirements.txt").read_text().splitlines()
+
+setup(
+    name="kok",
+    python_requires=">=3.8.0",
+    version="0.1.10",
+    description="Nvim completion source",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/kok.nvim",
+    packages=packages,
+    package_data=package_data,
+    install_requires=install_requires,
+)
