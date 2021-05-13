521a0ed2a499b03529fee36c9e8d513b661e368e
Fly me to the moon <git@bigly.dog>
2021-05-12 20:32:45 -0700

    left join

--- coq/server/model/sql/init/tables.sql
+++ coq/server/model/sql/init/tables.sql
@@ -56,24 +56,10 @@ CREATE INDEX IF NOT EXISTS word_locations_word     ON word_locations (word);
 CREATE INDEX IF NOT EXISTS word_locations_line_num ON word_locations (line_num);
 
 
--- !! files   1:N completions
--- Should be vaccumed by keeping last n rows, per source
-CREATE TABLE IF NOT EXISTS completions (
-  rowid         INTEGER NOT NULL PRIMARY KEY,
-  filename      TEXT    NOT NULL REFERENCES files    (filename) ON DELETE CASCADE,
-  response_time REAL,   -- if timeout -> NULL
-) WITHOUT ROWID;
-CREATE INDEX IF NOT EXISTS completions_source   ON completions (source);
-CREATE INDEX IF NOT EXISTS completions_filename ON completions (filename);
-
-
--- !! completions 1:1 insertions
 -- Stores insertion history
 -- Should be vacuumed by only keeping last n rows
--- Should be vacuumed by foreign key constraints on `completions`
 CREATE TABLE IF NOT EXISTS insertions (
   rowid         INTEGER NOT NULL PRIMARY KEY,
-  completion_id INTEGER NOT NULL UNIQUE REFERENCES completions (rowid) ON DELETE CASCADE,
   content       TEXT    NOT NULL
 ) WITHOUT ROWID;
 CREATE INDEX IF NOT EXISTS insertions_content ON insertions (content);
@@ -84,10 +70,22 @@ CREATE INDEX IF NOT EXISTS insertions_content ON insertions (content);
 --------------------------------------------------------------------------------
 
 
-CREATE VIEW IF NOT EXISTS words_by_insertion_view AS
+CREATE VIEW IF NOT EXISTS words_by_project_insertion_view AS
   SELECT
-    words.word AS word,
+    words.word       AS word,
+    files.filetype   AS filetype,
+    files.project    AS project,
+    insertions.rowid AS sort_by
   FROM words
+  JOIN word_locations
+  ON
+    word_locations.word = words.word
+  JOIN files
+  ON
+    files.filename = word_locations.filename
+  LEFT JOIN insertions
+  ON
+    insertions.content = word.word;
 
 
 CREATE VIEW IF NOT EXISTS words_by_project_filetype_view AS
