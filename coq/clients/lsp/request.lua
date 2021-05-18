e4d36dde72521d1d8490b891406bf1d2a98c01d4
Fly me to the moon <git@bigly.dog>
2021-05-18 04:24:10 -0700

    ok

--- coq/clients/lsp/request.lua
+++ coq/clients/lsp/request.lua
@@ -1,7 +1,3 @@
-local api = vim.api
-local fn = vim.fn
-local lsp = vim.lsp
-
 local cancel = function () end
 
 return function (chan_id, request_id, pos)
@@ -12,10 +8,10 @@ return function (chan_id, request_id, pos)
 
   local row, col = unpack(pos)
   local position = {line = row, character = col}
-  local text_doc = lsp.util.make_text_document_params()
+  local text_doc = vim.lsp.util.make_text_document_params()
   local params = {position = position, textDocument=text_doc}
 
-  _, cancel = lsp.buf_request(0, "textDocument/completion", params, function (_, _, resp)
+  _, cancel = vim.lsp.buf_request(0, "textDocument/completion", params, function (_, _, resp)
     vim.notify(chan_id, "", request_id, resp)
   end)
 end
