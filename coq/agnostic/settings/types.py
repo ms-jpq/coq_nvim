489da42009680e4bdcee386de8bd9c5c065f50fc
Fly me to the moon <git@bigly.dog>
2021-05-18 00:28:22 -0700

    ok

--- coq/agnostic/settings/types.py
+++ coq/agnostic/settings/types.py
@@ -3,22 +3,30 @@ from typing import AbstractSet
 
 
 @dataclass(frozen=True)
+class Display:
+    ellipsis: str
+    pum_max_len: int
+
+
+@dataclass(frozen=True)
 class Options:
-    unifying_chars: AbstractSet[str]
+    timeout: float
     transpose_band: int
+    unifying_chars: AbstractSet[str]
 
 
 @dataclass(frozen=True)
 class Weights:
-    insertion_order: float
-    count_by_filetype: float
-    nearest_neighbour: float
-    prefix_matches: float
     consecutive_matches: float
+    count_by_filetype: float
+    insertion_order: float
+    nearest_neighbour: float
     num_matches: float
+    prefix_matches: float
 
 
 @dataclass(frozen=True)
 class Settings:
-    match_options: Options
-    metric_weights: Weights
+    display: Display
+    match: Options
+    weights: Weights
