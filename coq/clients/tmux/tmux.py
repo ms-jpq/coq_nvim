cd53005540286e19811ac2f7c1a6b40dbb054a2a
Fly me to the moon <git@bigly.dog>
2021-05-18 02:56:17 -0700

    ok

--- coq/clients/tmux/tmux.py
+++ coq/clients/tmux/tmux.py
@@ -1,9 +1,9 @@
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from shutil import which
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 from time import sleep
-from typing import AbstractSet, Iterator, Mapping, Sequence
+from typing import AbstractSet, Iterator, Mapping, Optional, Sequence
 from uuid import UUID
 
 from ...shared.parse import coalesce
@@ -20,52 +20,65 @@ class _Pane:
     window_active: bool
 
 
-def _session() -> str:
-    out = check_output(("tmux", "display-message", "-p", "#{session_id}"), text=True)
-    return out.strip()
+def _session() -> Optional[str]:
+    try:
+        out = check_output(
+            ("tmux", "display-message", "-p", "#{session_id}"), text=True
+        )
+    except CalledProcessError:
+        return None
+    else:
+        return out.strip()
 
 
 def _panes() -> Sequence[_Pane]:
-    out = check_output(
-        (
-            "tmux",
-            "list-panes",
-            "-a",
-            "-F",
-            "#{session_id} #{pane_id} #{pane_active} #{window_active}",
-        ),
-        text=True,
-    )
+    try:
+        out = check_output(
+            (
+                "tmux",
+                "list-panes",
+                "-a",
+                "-F",
+                "#{session_id} #{pane_id} #{pane_active} #{window_active}",
+            ),
+            text=True,
+        )
 
-    def cont() -> Iterator[_Pane]:
-        for line in out.strip().splitlines():
-            session_id, pane_id, pane_active, window_active = line.split(" ")
-            pane = _Pane(
-                session_id=session_id,
-                pane_id=pane_id,
-                pane_active=bool(int(pane_active)),
-                window_active=bool(int(window_active)),
-            )
-            yield pane
+    except CalledProcessError:
+        return ()
+    else:
 
-    return tuple(cont())
+        def cont() -> Iterator[_Pane]:
+            for line in out.strip().splitlines():
+                session_id, pane_id, pane_active, window_active = line.split(" ")
+                pane = _Pane(
+                    session_id=session_id,
+                    pane_id=pane_id,
+                    pane_active=bool(int(pane_active)),
+                    window_active=bool(int(window_active)),
+                )
+                yield pane
+
+        return tuple(cont())
 
 
 def _screenshot(unifying_chars: AbstractSet[str], pane: _Pane) -> Sequence[str]:
-    out = check_output(("tmux", "capture-pane", "-p", "-t", pane.pane_id), text=True)
-    return tuple(coalesce(out, unifying_chars=unifying_chars))
-
-
-def _is_active(session_id: str, pane: _Pane) -> bool:
-    return session_id == pane.session_id and pane.pane_active and pane.window_active
+    try:
+        out = check_output(
+            ("tmux", "capture-pane", "-p", "-t", pane.pane_id), text=True
+        )
+    except CalledProcessError:
+        return ()
+    else:
+        return tuple(coalesce(out, unifying_chars=unifying_chars))
 
 
 def _collect(
     pool: ThreadPoolExecutor, unifying_chars: AbstractSet[str], session_id: str
 ) -> Mapping[_Pane, Sequence[str]]:
     panes = (pane for pane in _panes() if pane.session_id == session_id)
-    l1 = lambda pane: (pane, _screenshot(unifying_chars, pane=pane))
-    return {pane: words for pane, words in pool.map(l1, panes)}
+    cont = lambda pane: (pane, _screenshot(unifying_chars, pane=pane))
+    return {pane: words for pane, words in pool.map(cont, panes)}
 
 
 class Worker(BaseWorker[None]):
