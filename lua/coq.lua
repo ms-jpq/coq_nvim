62d0b49f14c91bd304c31c97d219203f9d7b5eaa
Fly me to the moon <git@bigly.dog>
2021-05-19 23:51:25 -0700

    ok

--- lua/coq.lua
+++ lua/coq.lua
@@ -1,5 +1,6 @@
 return function(args)
   local cwd = unpack(args)
+  local is_win = vim.api.nvim_call_function("has", {"win32"}) == 1
 
   local function defer(timeout, callback)
     local timer = vim.loop.new_timer()
@@ -16,26 +17,26 @@ return function(args)
   end
 
   local settings = function()
-    local go, _settings = pcall(vim.api.nvim_get_var, "Coq_settings")
+    local go, _settings = pcall(vim.api.nvim_get_var, "chadtree_settings")
     local settings = go and _settings or {}
     return settings
   end
 
-  Coq = Coq or {}
+  chad = chad or {}
   local linesep = "\n"
   local POLLING_RATE = 10
 
-  if Coq.loaded then
+  if chad.loaded then
     return
   else
-    Coq.loaded = true
+    chad.loaded = true
     local job_id = nil
-    local Coq_params = {}
+    local chad_params = {}
     local err_exit = false
 
-    Coq.on_exit = function(args)
+    chad.on_exit = function(args)
       local code = unpack(args)
-      local msg = " | Coq EXITED - " .. code
+      local msg = " | CHADTree EXITED - " .. code
       if not (code == 0 or code == 143) then
         err_exit = true
         vim.api.nvim_err_writeln(msg)
@@ -43,53 +44,78 @@ return function(args)
         err_exit = false
       end
       job_id = nil
-      for _, param in ipairs(Coq_params) do
-        Coq[Coq_params] = nil
+      for _, param in ipairs(chad_params) do
+        chad[chad_params] = nil
       end
     end
 
-    Coq.on_stdout = function(args)
+    chad.on_stdout = function(args)
       local msg = unpack(args)
       vim.api.nvim_out_write(table.concat(msg, linesep))
     end
 
-    Coq.on_stderr = function(args)
+    chad.on_stderr = function(args)
       local msg = unpack(args)
       vim.api.nvim_err_write(table.concat(msg, linesep))
     end
 
-    local start = function(...)
-      local is_win = vim.api.nvim_call_function("has", {"win32"}) == 1
-
+    local main = function(is_xdg)
       local go, _py3 = pcall(vim.api.nvim_get_var, "python3_host_prog")
       local py3 = go and _py3 or (is_win and "python" or "python3")
-      local main = cwd .. (is_win and [[\venv.bat]] or "/venv.sh")
+      local v_py =
+        cwd ..
+        (is_win and [[/.vars/runtime/Scripts/python.exe]] or
+          "/.vars/runtime/bin/python3")
+      local win_proxy = cwd .. [[/win.bat]]
+      local xdg_dir = vim.api.nvim_call_function("getenv", {"XDG_DATA_HOME"})
 
+      if is_win then
+        if vim.api.nvim_call_function("filereadable", {v_py}) == 1 then
+          return {v_py}
+        else
+          return {win_proxy, py3}
+        end
+      else
+        local v_py_xdg =
+          xdg_dir and (xdg_dir .. "/nvim/chadtree/runtime/bin/python3") or v_py
+        local v_py = is_xdg and v_py_xdg or v_py
+        if vim.api.nvim_call_function("filereadable", {v_py}) == 1 then
+          return {v_py}
+        else
+          return {py3}
+        end
+      end
+    end
+
+    local start = function(...)
+      local is_xdg = settings().xdg
       local args =
         vim.tbl_flatten {
-        {main, py3, "-m", "Coq"},
+        main(is_xdg),
+        {"-m", "chadtree"},
         {...},
-        (settings().xdg and {"--xdg"} or {})
+        (is_xdg and {"--xdg"} or {})
       }
       local params = {
-        on_exit = "Coqon_exit",
-        on_stdout = "Coqon_stdout",
-        on_stderr = "Coqon_stderr"
+        cwd = cwd,
+        on_exit = "CHADon_exit",
+        on_stdout = "CHADon_stdout",
+        on_stderr = "CHADon_stderr"
       }
       local job_id = vim.api.nvim_call_function("jobstart", {args, params})
       return job_id
     end
 
-    Coq.deps_cmd = function()
+    chad.deps_cmd = function()
       start("deps")
     end
 
-    vim.api.nvim_command [[command! -nargs=0 Coqdeps lua Coq.deps_cmd()]]
+    vim.api.nvim_command [[command! -nargs=0 CHADdeps lua chad.deps_cmd()]]
 
-    local set_Coq_call = function(name, cmd)
-      table.insert(Coq_params, name)
+    local set_chad_call = function(name, cmd)
+      table.insert(chad_params, name)
       local t1 = 0
-      Coq[name] = function(...)
+      chad[name] = function(...)
         local args = {...}
         if t1 == 0 then
           t1 = vim.loop.now()
@@ -114,7 +140,7 @@ return function(args)
               if err_exit then
                 return
               else
-                Coq[name](unpack(args))
+                chad[name](unpack(args))
               end
             end
           )
@@ -122,10 +148,10 @@ return function(args)
       end
     end
 
-    set_Coq_call("open_cmd", "Coqopen")
-    vim.api.nvim_command [[command! -nargs=* Coqopen lua Coq.open_cmd(<f-args>)]]
+    set_chad_call("open_cmd", "CHADopen")
+    vim.api.nvim_command [[command! -nargs=* CHADopen lua chad.open_cmd(<f-args>)]]
 
-    set_Coq_call("help_cmd", "Coqhelp")
-    vim.api.nvim_command [[command! -nargs=* Coqhelp lua Coq.help_cmd(<f-args>)]]
+    set_chad_call("help_cmd", "CHADhelp")
+    vim.api.nvim_command [[command! -nargs=* CHADhelp lua chad.help_cmd(<f-args>)]]
   end
 end
--- plugin/coq.vim
+++ plugin/coq.vim
@@ -1,12 +1,3 @@
-function Coqon_exit(_, code, __)
-  call luaeval('Coq.on_exit(...)', [a:code])
-endfunction
-function Coqon_stdout(_, msg, __)
-  call luaeval('Coq.on_stdout(...)', [a:msg])
-endfunction
-function Coqon_stderr(_, msg, __)
-  call luaeval('Coq.on_stderr(...)', [a:msg])
-endfunction
+let s:top_level = resolve(expand('<sfile>:p:h'))
 
-let s:top_level = resolve(expand('<sfile>:p:h:h'))
-call luaeval('require("Coq")(...)', [s:top_level])
+call luaeval('require("coq")(...)', [s:top_level])
