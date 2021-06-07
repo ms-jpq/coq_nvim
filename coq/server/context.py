8f14ed3b8f7085e568c6d6ca0690915767c2e5d4
Fly me to the moon <git@bigly.dog>
2021-06-07 15:29:12 -0700

    ok

--- coq/server/context.py
+++ coq/server/context.py
@@ -2,16 +2,21 @@ from typing import AbstractSet, Optional
 
 from pynvim import Nvim
 from pynvim.api import Buffer
-from pynvim_pp.api import buf_filetype, buf_name, cur_win, win_get_buf, win_get_cursor
+from pynvim_pp.api import (
+    buf_filetype,
+    buf_get_lines,
+    buf_name,
+    cur_win,
+    win_get_buf,
+    win_get_cursor,
+)
 from pynvim_pp.text_object import gen_split
 
 from ..shared.types import Context
-from .model.database import Database
 
 
 def context(
     nvim: Nvim,
-    db: Database,
     unifying_chars: AbstractSet[str],
     buf: Optional[Buffer],
     filename: Optional[str],
@@ -22,7 +27,7 @@ def context(
     row, col = win_get_cursor(nvim, win=win)
     pos = (row, col)
 
-    line, *_ = db.lines(buf=buf.number, lo=row, hi=row + 1)
+    line, *_ = buf_get_lines(nvim, buf=buf, lo=row, hi=row + 1)
     filename = filename if filename is not None else buf_name(nvim, buf=buf)
     filetype = filetype if filetype is not None else buf_filetype(nvim, buf=buf)
 
