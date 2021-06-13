d875e48b760801df7281398950d3e00926c67fc2
Fly me to the moon <git@bigly.dog>
2021-06-12 20:40:03 -0700

    timeout

--- coq/shared/runtime.py
+++ coq/shared/runtime.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import FIRST_EXCEPTION, ThreadPoolExecutor, TimeoutError, wait
 from threading import Lock
-from typing import Generic, MutableSequence, Sequence, TypeVar
-from uuid import UUID, uuid4
+from typing import Generic, Iterator, MutableSequence, MutableSet, Sequence, TypeVar
 from weakref import WeakSet
 
 from pynvim import Nvim
-from pynvim_pp.logging import log
 
 from .settings import Options
 from .types import Completion, Context
@@ -19,12 +17,7 @@ T_co = TypeVar("T_co", contravariant=True)
 class Supervisor:
     def __init__(self, nvim: Nvim, pool: ThreadPoolExecutor, options: Options) -> None:
         self._nvim, self._pool, self._options = nvim, pool, options
-
-        self._lock = Lock()
-        self._workers = WeakSet()
-
-        self._token = uuid4()
-        self._completions: MutableSequence[Completion] = []
+        self._workers: MutableSet[Worker] = WeakSet()
 
     @property
     def options(self) -> Options:
@@ -39,36 +32,26 @@ class Supervisor:
         return self._pool
 
     def register(self, worker: Worker) -> None:
-        with self._lock:
-            self._workers.add(worker)
+        self._workers.add(worker)
 
+    def work(self, context: Context) -> Sequence[Completion]:
+        lock = Lock()
+        acc: MutableSequence[Completion] = []
 
-    def report(self, token: UUID, completions: Sequence[Completion]) -> None:
-        with self._lock:
-            if token != self._token:
-                pass
-            else:
-                self._completions.extend(completions)
+        def supervise(worker: Worker) -> None:
+            for completion in worker.work(context):
+                with lock:
+                    acc.append(completion)
 
-    def exploit(self, context: Context) -> Sequence[Completion]:
-        with self._lock:
-            assert not self._completions
-
-            for worker in self._workers:
-
-                def cont() -> None:
-                    try:
-                        worker.work(self._token, context=context)
-                    except Exception as e:
-                        log.exception("%s", e)
-
-                self._pool.submit(cont)
-
-        with self._lock:
-            completions = self._completions
-            self._completions = []
-            self._token = uuid4()
-            return completions
+        futs = (self._pool.submit(supervise, worker) for worker in self._workers)
+        try:
+            wait(futs, return_when=FIRST_EXCEPTION, timeout=self._options.timeout)
+        except TimeoutError:
+            with lock:
+                return tuple(acc)
+        else:
+            with lock:
+                return tuple(acc)
 
 
 class Worker(Generic[T_co]):
@@ -77,5 +60,5 @@ class Worker(Generic[T_co]):
         self._supervisor.register(self)
 
     @abstractmethod
-    def work(self, token: UUID, context: Context) -> None:
+    def work(self, context: Context) -> Iterator[Completion]:
         ...
