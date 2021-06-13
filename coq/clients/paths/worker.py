2238215e2552d0ac8c14e63dab6e9a9e3a3ba9d1
Fly me to the moon <git@bigly.dog>
2021-06-13 15:43:48 -0700

    k

--- coq/clients/paths/worker.py
+++ coq/clients/paths/worker.py
@@ -6,29 +6,37 @@ from ...shared.runtime import Worker as BaseWorker
 from ...shared.types import Completion, Context, ContextualEdit
 
 
-def _parse(segments: Sequence[str]) -> Iterator[Tuple[str, Path]]:
-    if segments:
-        *lhs, rhs = segments
-        left_side = Path(*lhs)
-        entire = left_side / rhs
+def _p_lhs(lhs: str) -> str:
+    for sym in (".", "..", "~"):
+        if lhs.endswith(sym):
+            return sym
+    else:
+        return ""
 
-        if entire.is_dir():
-            prefix = str(entire)
-            try:
-                for path in entire.iterdir():
+
+def _parse(maybe_path: str) -> Iterator[Tuple[str, Path]]:
+    entire = Path(maybe_path)
+    lhs, go, rhs = maybe_path.rpartition(sep)
+    left_side = Path(lhs)
+
+    if entire.is_dir():
+        prefix = str(entire)
+        try:
+            for path in entire.iterdir():
+                yield prefix, path
+        except PermissionError:
+            pass
+    elif go and left_side.is_dir():
+        prefix = str(left_side)
+        try:
+            for path in left_side.iterdir():
+                if path.name.startswith(rhs):
                     yield prefix, path
-            except PermissionError:
-                pass
-        elif left_side.is_dir():
-            prefix = str(left_side)
-            try:
-                for path in left_side.iterdir():
-                    if path.name.startswith(rhs):
-                        yield prefix, path
-            except PermissionError:
-                pass
-        else:
-            yield from _parse(lhs)
+        except PermissionError:
+            pass
+    else:
+        yield from _parse(lhs)
+        pass
 
 
 class Worker(BaseWorker[None]):
@@ -36,9 +44,11 @@ class Worker(BaseWorker[None]):
         context.line_before
 
         def cont() -> Iterator[Completion]:
-            segments = context.line_before.split(sep)
-            if len(segments) > 1:
-                for prefix, path in _parse(segments):
+            lhs, go, rhs = context.line_before.rpartition(sep)
+            if go:
+                left = _p_lhs(lhs)
+                maybe_path = left + sep + rhs
+                for prefix, path in _parse(maybe_path):
                     new_text = str(path)
                     edit = ContextualEdit(
                         old_prefix=prefix, new_text=new_text, new_prefix=new_text
