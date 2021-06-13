97853976c1b68959cde216cd81a011c0b3bbba4b
Fly me to the moon <git@bigly.dog>
2021-06-13 16:06:03 -0700

    added test

--- tests/clients/paths/worker.py
+++ tests/clients/paths/worker.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 
+from ....coq.clients.paths.worker import parse
 
-class DoubleEndedIterator(TestCase):
+
+class Parser(TestCase):
     def test_1(self) -> None:
-        t1 = (1, 2, 3, 4, 5)
-        d1 = deiter(t1)
-        t2 = tuple(d1)
-        self.assertEqual(t1, t2)
+        line = "abc~/cdf"
+        self.assertEqual(1, 1)
+
