be17e9bda34494489e4190615a4af73c802666f3
Fly me to the moon <git@bigly.dog>
2021-06-16 14:22:11 -0700

    okie

--- coq/clients/tmux/sql/select/words.sql
+++ coq/clients/tmux/sql/select/words.sql
@@ -2,11 +2,11 @@ SELECT DISTINCT
   word
 FROM words
 WHERE
-  LENGTH(:word) >= (:prefix_len - 1)
+  LENGTH(:word) >= :prefix_len
   AND
   pane_id <> :pane_id
   AND
-  lword LIKE X_LIKE_ESC(SUBSTR(X_LOWER(:word), 1, :prefix_len - 1)) ESCAPE '!'
+  lword LIKE X_LIKE_ESC(SUBSTR(X_LOWER(:word), 1, :prefix_len)) ESCAPE '!'
   AND
   NOT INSTR(:word, word)
 
--- coq/server/model/sql/select/words_by_prefix.sql
+++ coq/server/model/sql/select/words_by_prefix.sql
@@ -2,10 +2,10 @@ SELECT DISTINCT
   word
 FROM words
 WHERE
-  LENGTH(:word) >= (:prefix_len - 1)
+  LENGTH(:word) >= :prefix_len
   AND
   filename LIKE X_LIKE_ESC(:cwd) ESCAPE '!'
   AND
-  lword LIKE X_LIKE_ESC(SUBSTR(X_LOWER(:word), 1, :prefix_len - 1)) ESCAPE '!'
+  lword LIKE X_LIKE_ESC(SUBSTR(X_LOWER(:word), 1, :prefix_len)) ESCAPE '!'
   AND
   NOT INSTR(:word, word)
