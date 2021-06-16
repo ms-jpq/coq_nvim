5f3f2c2f6cc8dd5bb15c50e8d756b05db6a9858b
Fly me to the moon <git@bigly.dog>
2021-06-16 08:47:44 -0700

    non blocking rpc

--- coq/server/registrants/notify.py
+++ coq/server/registrants/notify.py
@@ -7,7 +7,7 @@ from ...registry import rpc
 from ..runtime import Stack
 
 
-@rpc(blocking=True)
+@rpc(blocking=False)
 def notify(nvim: Nvim, stack: Stack, uid: str, *msg: Any) -> None:
     uuid = UUID(uid)
     stack.supervisor.notify(uuid, msg)
