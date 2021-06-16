5c7b701d4383685fdf939be5a1b82f53c8102f38
Fly me to the moon <git@bigly.dog>
2021-06-16 14:19:06 -0700

    ok

--- coq/clients/tmux/sql/select/words.sql
+++ coq/clients/tmux/sql/select/words.sql
@@ -2,6 +2,8 @@ SELECT DISTINCT
   word
 FROM words
 WHERE
+  LEN(:word) >= :prefix_len - 1
+  AND
   pane_id <> :pane_id
   AND
   lword LIKE X_LIKE_ESC(SUBSTR(X_LOWER(:word), 1, :prefix_len - 1)) ESCAPE '!'
