80ae828f988444a616c4f4c03c7ae9bdd3968453
Fly me to the moon <git@bigly.dog>
2021-06-18 12:14:39 -0700

    no lsp wait

--- coq/clients/lsp/request.lua
+++ coq/clients/lsp/request.lua
@@ -10,20 +10,26 @@
     session = session_id
     cancel = nil
 
-    local row, col = unpack(pos)
-    local position = {line = row, character = col}
-    local text_doc = vim.lsp.util.make_text_document_params()
-    local params = {position = position, textDocument = text_doc}
+    local clients = vim.lsp.buf_get_clients(0)
+    if #clients == 0 then
+      COQnotify(request_id, vim.NIL)
+    else
+      local row, col = unpack(pos)
+      local position = {line = row, character = col}
+      local text_doc = vim.lsp.util.make_text_document_params()
+      local params = {position = position, textDocument = text_doc}
 
-    _, cancel =
-      vim.lsp.buf_request(
-      0,
-      "textDocument/completion",
-      params,
-      function(_, _, resp)
-        COQnotify(request_id, resp or vim.NIL)
-      end
-    )
+      local _ = nil
+      _, cancel =
+        vim.lsp.buf_request(
+        0,
+        "textDocument/completion",
+        params,
+        function(_, _, resp)
+          COQnotify(request_id, resp or vim.NIL)
+        end
+      )
+    end
   end
 end)(...)
 
