6371f247a0d0a6a6b5a36b36c82b6529c3d75f11
Fly me to the moon <git@bigly.dog>
2021-06-19 21:17:33 -0700

    ok

--- coq/ci/main.py
+++ coq/ci/main.py
@@ -4,11 +4,11 @@ from std2.pickle import encode
 from std2.tree import recur_sort
 
 from ..consts import SNIPPET_ARTIFACTS
-from .load import load
+from .load import load_parsable
 
 
 def main() -> None:
-    parsed = load()
+    parsed = load_parsable()
     ordered = recur_sort(encode(parsed))
     json = dumps(ordered, check_circular=False, ensure_ascii=False, indent=2)
     SNIPPET_ARTIFACTS.write_text(json)
