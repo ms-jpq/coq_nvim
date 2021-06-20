5b5cc918e07c109dc9c8b9855b9c1eff62db15b9
Fly me to the moon <git@bigly.dog>
2021-06-19 23:05:51 -0700

    sql

--- coq/clients/snippet/sql/select/snippet.sql
+++ coq/clients/snippet/sql/select/snippet.sql
@@ -1,23 +1,25 @@
 SELECT
   snippets.rowid AS snippet_id,
   snippets.grammar AS grammar,
-  snippets.match AS match,
   snippets.content AS content,
   snippets.label AS label,
   snippets.doc AS doc
 FROM snippets
 JOIN matches
 ON matches.snippet_id = snippets.rowid
+JOIN (
+  SELECT DISTINCT
+    dest AS filetype
+  FROM extensions_view
+  WHERE
+    src = :filetype
+  ) AS filetypes
+ON
+  snippets.filetype = filetypes.filetype
 WHERE
   :word <> ''
   AND
-  snippets.filetype IN (
-    SELECT
-      dest
-    FROM extensions_view
-    WHERE
-      src = :filetype
-  )
+  snippets.filetype IN
   AND
-  snippets.lmatch LIKE X_LIKE_ESC(X_LOWER(:word)) ESCAPE '!'
+  matches.lmatch LIKE X_LIKE_ESC(X_LOWER(:word)) ESCAPE '!'
 
