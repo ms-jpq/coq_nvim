24bce06fbfbf32ab0bfdfc66b32d3c731584b2a4
Fly me to the moon <git@bigly.dog>
2021-06-19 18:35:40 -0700

    okie

--- tests/snippets/parse.py
+++ tests/snippets/parse.py
@@ -1,13 +1,54 @@
+from sys import stderr
+from typing import Iterator
 from unittest import TestCase
+from uuid import uuid4
 
 from ...coq.ci.load import load
+from ...coq.shared.types import Context, EditEnv, SnippetEdit
+from ...coq.snippets.parse import parse
 
 
 class Parser(TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls._specs = load()
-
     def test_1(self) -> None:
-        pass
+        ctx = Context(
+            uid=uuid4(),
+            cwd="",
+            filename="",
+            filetype="",
+            position=(0, 0),
+            line="",
+            line_before="",
+            line_after="",
+            words="",
+            words_before="",
+            words_after="",
+            syms="",
+            syms_before="",
+            syms_after="",
+        )
+        env = EditEnv(linefeed="\n", tabstop=4, expandtab=True)
+
+        def cont() -> Iterator[SnippetEdit]:
+            specs = load()
+            for snippets in specs.snippets.values():
+                for snippet in snippets:
+                    edit = SnippetEdit(
+                        new_text=snippet.content,
+                        grammar=snippet.grammar,
+                    )
+                    yield edit
+
+        edits = tuple(cont())
+
+        def errs() -> Iterator[Exception]:
+            for edit in edits:
+                try:
+                    parse(ctx, env=env, snippet=edit)
+                except Exception as e:
+                    yield e
+
+        errors = tuple(errs())
+
+        succ = len(errors) / len(edits)
+        self.assertGreater(succ, 0.95)
 
