e58e8c370c81895e059466569478930302c0882f
Fly me to the moon <git@bigly.dog>
2021-06-20 00:52:49 -0700

    typo

--- coq/server/model/snippets/sql/insert/snippet.sql
+++ coq/server/model/snippets/sql/insert/snippet.sql
@@ -1,2 +1,2 @@
-INSERT INTO snippets ( rowid          filetype,          grammar,          content,          label,          doc)
+INSERT INTO snippets ( rowid,         filetype,          grammar,          content,          label,          doc)
 VALUES               (:rowid, X_NORM(:filetype), X_NORM(:grammar), X_NORM(:content), X_NORM(:label), X_NORM(:doc))
