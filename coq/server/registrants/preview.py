f53ac051c3a694306cd60c409b9b7cbb7b5ab0b1
I love my dog <git@bigly.dog>
2021-06-24 03:20:02 -0700

    lua

--- coq/server/registrants/preview.py
+++ coq/server/registrants/preview.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from functools import partial
 from pathlib import Path
-from typing import Any, Mapping, Sequence
+from typing import Any, Callable, Mapping, Sequence
 
 from pynvim import Nvim
 from pynvim.api import Window
@@ -43,6 +43,10 @@ class _Pos:
 _MARGIN = 4
 
 
+def _clamp(hi: int) -> Callable[[int], int]:
+    return lambda i: min(hi, i) - _MARGIN
+
+
 def _positions(nvim: Nvim, event: _Event, lines: Sequence[str]) -> Sequence[_Pos]:
     t_height, t_width = nvim.options["lines"], nvim.options["columns"]
     top, btm, left, right = (
@@ -51,86 +55,93 @@ def _positions(nvim: Nvim, event: _Event, lines: Sequence[str]) -> Sequence[_Pos
         event.col,
         event.col + event.width + 1,
     )
-    limit_h, limit_w = partial(min, len(lines)), partial(min, max(map(len, lines)))
-    limit_h = limit_w = lambda x: x
+    limit_h, limit_w = _clamp(len(lines)), _clamp(max(map(len, lines)))
 
     ns_width = limit_w(t_width - right)
     n_height = limit_h(top - 1)
 
     ns_col = left - 1
     n = _Pos(
-        row=top + 1 - n_height,
+        row=top - 1 - n_height,
         col=ns_col,
         height=n_height,
         width=ns_width,
     )
 
     s = _Pos(
-        row=btm + 1,
+        row=btm + 2,
         col=ns_col,
         height=limit_h(t_height - btm),
         width=ns_width,
     )
 
     we_height = limit_h(t_height - top)
-    w_width = limit_w(left)
+    w_width = limit_w(left - 1)
 
     w = _Pos(
         row=top,  # OK
-        col=left - w_width,
+        col=left - w_width - 1,
         height=we_height,
         width=w_width,
     )
 
     e = _Pos(
         row=top,  # OK
-        col=right + 1,  # OK
+        col=right + 2,  # OK
         height=we_height,
-        width=limit_w(t_width - right),
+        width=limit_w(t_width - right - 2),
     )
     return (n, s, w, e)
 
 
+def _preview(nvim: Nvim, event: _Event, doc: Doc) -> None:
+    lines = doc.text.splitlines()
+    pos, *_ = sorted(
+        _positions(nvim, event=event, lines=lines),
+        key=lambda p: p.height * p.width,
+        reverse=True,
+    )
+    opts = {
+        "relative": "editor",
+        "anchor": "NW",
+        "style": "minimal",
+        "width": pos.width,
+        "height": pos.height,
+        "row": pos.row,
+        "col": pos.col,
+    }
+    buf = create_buf(
+        nvim, listed=False, scratch=True, wipe=True, nofile=True, noswap=True
+    )
+    buf_set_preview(nvim, buf=buf, filetype=doc.filetype, preview=lines)
+    win: Window = nvim.api.open_win(buf, False, opts)
+    win_set_option(nvim, win=win, key="wrap", val=True)
+    win_set_option(nvim, win=win, key="foldenable", val=False)
+    # win_set_option(nvim, win=win, key="winhighlight", val="Normal:Floating")
+
+
 @rpc(blocking=True)
-def _d_preview(nvim: Nvim, stack: Stack, event: Mapping[str, Any] = {}) -> None:
+def _cmp_changed(nvim: Nvim, stack: Stack, event: Mapping[str, Any] = {}) -> None:
     try:
         ev: _Event = decode(_Event, event)
         data: UserData = decode(
             UserData, ev.completed_item.user_data, decoders=BUILTIN_DECODERS
         )
-    except DecodeError as e:
+    except DecodeError:
         pass
     else:
         if data and data.doc and data.doc.text:
-            lines = data.doc.text.splitlines()
-            pos, *_ = sorted(
-                _positions(nvim, event=ev, lines=lines),
-                key=lambda p: p.height * p.width,
-                reverse=True,
-            )
-            opts = {
-                "relative": "editor",
-                "anchor": "NW",
-                "style": "minimal",
-                "width": pos.width,
-                "height": pos.height,
-                "row": pos.row,
-                "col": pos.col,
-            }
-            buf = create_buf(
-                nvim, listed=False, scratch=True, wipe=True, nofile=True, noswap=True
-            )
-            buf_set_preview(nvim, buf=buf, filetype=data.doc.filetype, preview=lines)
-            win: Window = nvim.api.open_win(buf, False, opts)
-            win_set_option(nvim, win=win, key="wrap", val=True)
-            win_set_option(nvim, win=win, key="foldenable", val=False)
-            # win_set_option(nvim, win=win, key="winhighlight", val="Normal:Floating")
+            _preview(nvim, event=ev, doc=data.doc)
 
 
-@rpc(blocking=True)
-def _cmp_changed(nvim: Nvim, stack: Stack, event: Mapping[str, Any] = {}) -> None:
-    nvim.api.exec_lua("COQpreview(...)", (event,))
+_LUA = f"""
+(function()
+  local event = vim.v.event
+  vim.schedule(function() 
+    {_cmp_changed.name}(event)
+  end)
+end)()
+"""
 
-
-autocmd("CompleteChanged") << f"lua {_cmp_changed.name}(vim.v.event)"
+autocmd("CompleteChanged") << f"lua {_LUA}"
 
