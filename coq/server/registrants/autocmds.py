4baf7bf00f528a51f4dec34fea254f0701c38a9e
I love my dog <git@bigly.dog>
2021-06-25 14:14:02 -0700

    based

--- coq/server/registrants/autocmds.py
+++ coq/server/registrants/autocmds.py
@@ -6,6 +6,7 @@ from pynvim_pp.api import buf_filetype, buf_name, cur_buf
 from std2.pickle import decode
 
 from ...registry import atomic, autocmd, rpc
+from ...shared.timeit import timeit
 from ...snippets.types import ParsedSnippet
 from ..runtime import Stack
 
@@ -30,13 +31,14 @@ def _ft_changed(nvim: Nvim, stack: Stack) -> None:
     stack.bdb.ft_update(name, filetype=ft)
 
     if ft not in _SEEN:
-        _SEEN.add(ft)
-        snippets = stack.settings.clients.snippets
-        mappings = {
-            f: decode(Sequence[ParsedSnippet], snippets.snippets.get(f, ()))
-            for f in chain(snippets.extends.get(ft, {}).keys(), (ft,))
-        }
-        stack.sdb.populate(mappings)
+        with timeit("NEW SNIPPETS"):
+            _SEEN.add(ft)
+            snippets = stack.settings.clients.snippets
+            mappings = {
+                f: decode(Sequence[ParsedSnippet], snippets.snippets.get(f, ()))
+                for f in chain(snippets.extends.get(ft, {}).keys(), (ft,))
+            }
+            stack.sdb.populate(mappings)
 
 
 autocmd("FileType") << f"lua {_ft_changed.name}()"
