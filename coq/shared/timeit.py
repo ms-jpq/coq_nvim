41c657da5d7774ff0dc5a99e34336b2b70383054
I love my dog <git@bigly.dog>
2021-06-25 01:52:25 -0700

    better timeit

--- coq/shared/timeit.py
+++ coq/shared/timeit.py
@@ -1,17 +1,31 @@
 from contextlib import contextmanager
-from typing import Any, Iterator
+from typing import Any, Iterator, MutableMapping, Tuple
 
 from pynvim_pp.logging import log
 from std2.locale import si_prefixed_smol
 from std2.timeit import timeit as _timeit
 
+from ..consts import DEBUG
+
+_RECORDS: MutableMapping[str, Tuple[int, float]] = {}
+
 
 @contextmanager
 def timeit(name: str, *args: Any) -> Iterator[None]:
-    with _timeit() as t:
+    if DEBUG:
+        with _timeit() as t:
+            yield None
+        delta = t()
+
+        times, cum = _RECORDS.get(name, (0, 0))
+        t, c = times + 1, cum + delta
+        _RECORDS[name] = t, c
+
+        label = name.ljust(16)
+        time = f"{si_prefixed_smol(delta, precision=0)}s".ljust(8)
+        ttime = f"{si_prefixed_smol(c / t, precision=0)}s".ljust(8)
+        msg = f"TIME -- {label} :: {time} & {ttime} {' '.join(args)}"
+        log.debug("%s", msg)
+    else:
         yield None
-    delta = t()
-    time = f"{si_prefixed_smol(delta)}s".ljust(8)
-    msg = f"TIME -- {name.ljust(9)} :: {time} {' '.join(args)}"
-    log.debug("%s", msg)
 
