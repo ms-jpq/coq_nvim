e90181f6826a1604a0e72884e8a9554bb2b399af
I love my dog <git@bigly.dog>
2021-06-24 21:40:03 -0700

    ok

--- coq/shared/timeit.py
+++ coq/shared/timeit.py
@@ -2,6 +2,7 @@ from contextlib import contextmanager
 from typing import Any, Iterator
 
 from pynvim_pp.logging import log
+from std2.locale import si_prefixed_smol
 from std2.timeit import timeit as _timeit
 
 
@@ -10,7 +11,8 @@ def timeit(threshold: float, name: str, *args: Any) -> Iterator[None]:
     with _timeit() as t:
         yield None
     delta = t()
+    time = si_prefixed_smol(delta, precision=3)
     if delta > threshold:
-        msg = f"TIME -- {name.ljust(9)} :: {format(delta, '0.3f')} {' '.join(args)}"
+        msg = f"TIME -- {name.ljust(9)} :: {time} {' '.join(args)}"
         log.debug("%s", msg)
 
