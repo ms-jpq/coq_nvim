3fa191b71cbfbd829af3317a2db5df93df4c15d0
Fly me to the moon <git@bigly.dog>
2021-06-26 02:28:54 -0700

    ignore

--- coq/server/model/snippets/sql/insert/snippet.sql
+++ coq/server/model/snippets/sql/insert/snippet.sql
@@ -1,2 +1,2 @@
-INSERT INTO snippets ( rowid,              filetype,               grammar,               content,               label,               doc)
-VALUES               (:rowid, X_NORMALIZE(:filetype), X_NORMALIZE(:grammar), X_NORMALIZE(:content), X_NORMALIZE(:label), X_NORMALIZE(:doc))
+INSERT OR IGNORE INTO snippets ( rowid,              filetype,               grammar,               content,               label,               doc)
+VALUES                         (:rowid, X_NORMALIZE(:filetype), X_NORMALIZE(:grammar), X_NORMALIZE(:content), X_NORMALIZE(:label), X_NORMALIZE(:doc))
