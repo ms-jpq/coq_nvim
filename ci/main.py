a26a5911f35d9b0b912c02e6cc94f968501bbd0d
I love my dog <git@bigly.dog>
2021-06-26 05:13:18 -0700

    cwd

--- ci/main.py
+++ ci/main.py
@@ -48,14 +48,14 @@ def _git_alert(cwd: Path) -> None:
     if refs:
         check_call(("git", "push", "--delete", "origin", *refs), cwd=cwd)
 
-    proc = run(("git", "diff", "--exit-code"))
+    proc = run(("git", "diff", "--exit-code"), cwd=cwd)
     if proc.returncode:
         time = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d")
         brname = f"{prefix}--{time}"
-        check_call(("git", "checkout", "-b", brname))
+        check_call(("git", "checkout", "-b", brname), cwd=cwd)
         check_call(("git", "add", "."))
-        check_call(("git", "commit", "-m", f"update_artifacts: {time}"))
-        check_call(("git", "push", "--set-upstream", "origin", brname))
+        check_call(("git", "commit", "-m", f"update_artifacts: {time}"), cwd=cwd)
+        check_call(("git", "push", "--set-upstream", "origin", brname), cwd=cwd)
 
 
 def main() -> None:
