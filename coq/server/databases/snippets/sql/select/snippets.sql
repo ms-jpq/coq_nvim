4b7190212ee97c34a64de4cd975268d35a37759a
dog <github@reeee.ca>
2021-07-03 11:51:13 -0700

    or ignore

--- coq/server/databases/snippets/sql/select/snippets.sql
+++ coq/server/databases/snippets/sql/select/snippets.sql
@@ -18,4 +18,5 @@ WHERE
   matches.lmatch LIKE X_LIKE_ESC(X_LOWER(X_NORMALIZE(SUBSTR(:word, 1, :exact)))) ESCAPE '!'
   AND
   X_SIMILARITY(X_LOWER(X_NORMALIZE(SUBSTR(:word, 1, :exact))), matches.lmatch) > :cut_off
-
+GROUP BY
+  snippets.rowid
