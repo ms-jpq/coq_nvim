798e202ffbb7eda8021bd0f4c5b734110d3ffd87
Fly me to the moon <git@bigly.dog>
2021-07-03 14:04:08 -0700

    ok

--- coq/lsp/requests/request.py
+++ coq/lsp/requests/request.py
@@ -46,7 +46,7 @@ def blocking_request(nvim: Nvim, method: str, *args: Any) -> Iterator[Any]:
         ev.wait()
         with _LOCK:
             ses, done, acc = _STATE.get(method, ("", True, ()))
-        ev.clear()
+            ev.clear()
         if ses != session:
             break
         else:
