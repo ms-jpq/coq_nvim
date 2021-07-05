c5ef84968592fca6373610ac85f471b842d7192c
dog <github@reeee.ca>
2021-07-04 21:47:31 -0700

    added groupby

--- coq/server/databases/insertions/sql/select/metrics.sql
+++ coq/server/databases/insertions/sql/select/metrics.sql
@@ -1,9 +1,12 @@
 SELECT
+  candidates.rowid                 AS idx,
   candidates.sort_by               AS sort_by,
   COALESCE(MAX(inserted.rowid), 0) AS insert_order
 FROM candidates
 LEFT JOIN inserted
 ON
   inserted.sort_by = candidates.sort_by
+GROUP BY
+  idx
 ORDER BY
-  candidates.rowid
+  idx
