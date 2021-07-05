51dd41fe972f4582567bd8e846d8948780202688
dog <github@reeee.ca>
2021-07-04 17:51:04 -0700

    cool beans

--- coq/server/databases/insertions/sql/create/tables.sql
+++ coq/server/databases/insertions/sql/create/tables.sql
@@ -16,23 +16,25 @@ CREATE TABLE IF NOT EXISTS durations (
   duration REAL    NOT NULL,
   items    INTEGER NOT NULL
 );
+CREATE INDEX IF NOT EXISTS durations_batch_id ON durations (batch_id);
 
 
 CREATE TABLE IF NOT EXISTS inserted (
   rowid     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   source_id TEXT    NOT NULL REFERENCES sources (name)  ON UPDATE CASCADE ON DELETE CASCADE,
-  batch_id  INTEGER NOT NULL REFERENCES batch   (rowid) ON UPDATE CASCADE ON DELETE CASCADE,
-  sort_by   TEXT    NOT NULL UNIQUE,
+  sort_by   TEXT    NOT NULL
   UNIQUE(source_id, batch_id)
 );
+CREATE INDEX IF NOT EXISTS inserted_source_id ON inserted (source_id);
 
 
 CREATE TABLE IF NOT EXISTS candidates (
   rowid     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  batch_id  INTEGER NOT NULL REFERENCES batch   (rowid) ON UPDATE CASCADE ON DELETE CASCADE,
+  batch_id  INTEGER NOT NULL REFERENCES batch (rowid) ON UPDATE CASCADE ON DELETE CASCADE,
   sort_by   TEXT    NOT NULL UNIQUE,
   UNIQUE(source_id, batch_id)
 );
+CREATE INDEX IF NOT EXISTS candidates_batch_id ON candidates (batch_id);
 
 
 END;
