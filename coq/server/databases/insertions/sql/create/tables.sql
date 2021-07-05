4751caf0a7a45598db3addf58f7878667cafec59
dog <github@reeee.ca>
2021-07-04 20:58:27 -0700

    blob

--- coq/server/databases/insertions/sql/create/tables.sql
+++ coq/server/databases/insertions/sql/create/tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS sources (
 
 
 CREATE TABLE IF NOT EXISTS batches (
-  rowid     INTEGER NOT NULL PRIMARY KEY,
+  rowid     BLOB    NOT NULL PRIMARY KEY,
   source_id TEXT    NOT NULL REFERENCES sources (name) ON UPDATE CASCADE ON DELETE CASCADE,
   duration  REAL    NOT NULL,
   items     INTEGER NOT NULL
@@ -17,7 +17,7 @@ CREATE INDEX IF NOT EXISTS batches_source_id ON batches (source_id);
 
 CREATE TABLE IF NOT EXISTS inserted (
   rowid    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  batch_id INTEGER NOT NULL REFERENCES batches (rowid) ON UPDATE CASCADE ON DELETE CASCADE,
+  batch_id BLOB    NOT NULL REFERENCES batches (rowid) ON UPDATE CASCADE ON DELETE CASCADE,
   sort_by  TEXT    NOT NULL
 );
 CREATE INDEX IF NOT EXISTS inserted_batch_id ON inserted (batch_id);
@@ -25,8 +25,8 @@ CREATE INDEX IF NOT EXISTS inserted_sort_by  ON inserted (sort_by);
 
 
 CREATE TABLE IF NOT EXISTS candidates (
-  rowid     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  sort_by   TEXT    NOT NULL
+  rowid   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  sort_by TEXT    NOT NULL
 );
 CREATE INDEX IF NOT EXISTS candidates_sort_by ON candidates (sort_by);
 
