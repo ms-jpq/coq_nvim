c1d89de57e6f81f866234138880e11205c63da46
DOG DOG DOG! <github@bigly.dog>
2021-07-10 13:04:34 -0700

    oops

--- coq/server/registrants/omnifunc.py
+++ coq/server/registrants/omnifunc.py
@@ -94,7 +94,7 @@ def _comp_done(nvim: Nvim, stack: Stack, event: Mapping[str, Any]) -> None:
         else:
             s = state()
             if user_data.change_uid == s.change_id:
-                inserted = edit(nvim, stack=stack, state=state, data=user_data)
+                inserted = edit(nvim, stack=stack, state=s, data=user_data)
                 state(inserted=inserted, commit_id=uuid4())
             else:
                 log.warn("%s", "delayed completion")
