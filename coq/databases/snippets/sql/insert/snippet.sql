03b73ca5266c950018b10be491ebe0be167905d5
DOG DOG DOG! <github@bigly.dog>
2021-07-10 12:57:03 -0700

    remove normalization

--- coq/databases/snippets/sql/insert/snippet.sql
+++ coq/databases/snippets/sql/insert/snippet.sql
@@ -1,3 +1,3 @@
-INSERT OR IGNORE INTO snippets ( rowid,  filetype,  grammar   content   label   doc)
+INSERT OR IGNORE INTO snippets ( rowid,  filetype,  grammar,  content,  label,  doc)
 VALUES                         (:rowid, :filetype, :grammar, :content, :label, :doc)
 
