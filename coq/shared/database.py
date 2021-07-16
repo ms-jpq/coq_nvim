34ca6bfc9759a97505a2837debffc4c436cd93bf
DOG DOG DOG! <github@bigly.dog>
2021-07-16 13:58:20 -0700

    use multimode

--- coq/shared/database.py
+++ coq/shared/database.py
@@ -10,11 +10,12 @@ def _like_esc(like: str) -> str:
 
 
 def _similarity(lhs: str, rhs: str) -> float:
-    l, r = Counter(lhs), Counter(rhs)
-    a = l - r if len(lhs) > len(rhs) else r - l
-    t = sum(a.values())
-    m = max(len(lhs), len(rhs))
-    return t / m
+    l_c, r_c = Counter(lhs), Counter(rhs)
+    dif = l_c - r_c if len(lhs) > len(rhs) else r_c - l_c
+    bigger, smaller = max(len(lhs), len(rhs)), min(len(lhs), len(rhs))
+    ratio = 1 - sum(dif.values()) / bigger
+    adjust = smaller / bigger
+    return ratio / adjust
 
 
 def init_db(conn: Connection) -> None:
