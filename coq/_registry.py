88dd70c8ff365e9f90b8a52c18b1a1ffdcb2276d
DOG DOG DOG! <github@bigly.dog>
2021-07-18 06:25:36 -0700

    help now operation

--- coq/_registry.py
+++ coq/_registry.py
@@ -1,6 +1,7 @@
 from .lsp.requests import completion, preview, request
-from .server.registrants import attachment, autocmds, marks, noop, omnifunc, stats
+from .server.registrants import attachment, autocmds, help, marks, noop, omnifunc
 from .server.registrants import preview as rp
+from .server.registrants import stats
 
 ____ = None
 
