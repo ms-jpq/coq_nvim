422b535f866cef26db86ed828bec5b1fc252ffaa
DOG DOG DOG! <github@bigly.dog>
2021-07-18 22:31:30 -0700

    quick tests

--- tests/shared/fuzzy.py
+++ tests/shared/fuzzy.py
@@ -10,6 +10,24 @@ class QuickRatio(TestCase):
         ratio = quick_ratio(lhs, rhs)
         self.assertAlmostEqual(ratio, 1)
 
+    def test_2(self) -> None:
+        lhs = "ac"
+        rhs = "ab"
+        ratio = quick_ratio(lhs, rhs)
+        self.assertAlmostEqual(ratio, 0.5)
+
+    def test_3(self) -> None:
+        lhs = "acb"
+        rhs = "abc"
+        ratio = quick_ratio(lhs, rhs)
+        self.assertAlmostEqual(ratio, 1)
+
+    def test_4(self) -> None:
+        lhs = "abc"
+        rhs = "abz"
+        ratio = quick_ratio(lhs, rhs)
+        self.assertAlmostEqual(ratio, 2 / 3)
+
 
 class Metrics(TestCase):
     def test_1(self) -> None:
