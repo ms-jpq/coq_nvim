7b64b78e5fa7de84ee50f0352afaf232fb459450
DOG DOG DOG! <github@bigly.dog>
2021-07-18 23:30:56 -0700

    what

--- tests/shared/fuzzy.py
+++ tests/shared/fuzzy.py
@@ -1,43 +1,55 @@
 from unittest import TestCase
 
-from ...coq.shared.fuzzy import metrics, osa_distance, quick_ratio
+from ...coq.shared.fuzzy import dl_distance, metrics, quick_ratio
 
 
 class EditD(TestCase):
     def test_1(self) -> None:
         lhs = ""
         rhs = ""
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 0)
 
     def test_2(self) -> None:
         lhs = "a"
         rhs = "b"
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 1)
 
     def test_3(self) -> None:
         lhs = "ca"
         rhs = "abc"
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 2)
 
     def test_4(self) -> None:
         lhs = "cac"
         rhs = "aca"
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 2)
 
     def test_5(self) -> None:
         lhs = "cacaca"
         rhs = "acacac"
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 3)
 
     def test_6(self) -> None:
         lhs = ""
         rhs = "abc"
-        d = osa_distance(lhs, rhs)
+        d = dl_distance(lhs, rhs)
+        self.assertEqual(d, 3)
+
+    def test_7(self) -> None:
+        lhs = "ab"
+        rhs = "bca"
+        d = dl_distance(lhs, rhs)
+        self.assertEqual(d, 2)
+
+    def test_8(self) -> None:
+        lhs = "badc"
+        rhs = "abcd"
+        d = dl_distance(lhs, rhs)
         self.assertEqual(d, 3)
 
 
