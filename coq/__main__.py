1fff79b10a5e192f869c7ee67e8ef833bba07c42
DOG DOG DOG <github@bigly.dog>
2021-07-21 14:41:01 -0700

    ok

--- coq/__main__.py
+++ coq/__main__.py
@@ -7,7 +7,7 @@ from sys import executable, stderr, stdout, version_info
 from textwrap import dedent
 from typing import Union
 
-from .consts import LSP_ARTIFACTS, REQUIREMENTS, RT_DIR, RT_PY, TOP_LEVEL, VARS
+from .consts import REQUIREMENTS, RT_DIR, RT_PY, TOP_LEVEL, VARS
 
 if version_info < (3, 8, 2):
     msg = "⛔️ python < 3.8.2"
