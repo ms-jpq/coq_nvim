7ed256c99793d70e5e6a8f75fd888eb696ccc148
DOG DOG DOG <github@bigly.dog>
2021-07-21 11:58:31 -0700

    ok

--- coq/snippets/artifacts.py
+++ coq/snippets/artifacts.py
@@ -1,9 +1,7 @@
 from json import loads
 
 from ..consts import SNIPPET_ARTIFACTS
-from ..shared.timeit import timeit
 from .types import ASnips
 
-with timeit("LOAD-SNIPS"):
-    SNIPPETS: ASnips = loads(SNIPPET_ARTIFACTS.read_text("UTF-8"))
+SNIPPETS: ASnips = loads(SNIPPET_ARTIFACTS.read_text("UTF-8"))
 
