a989122138218b2b6c212ae653456e297a294d29
DOG DOG DOG <github@bigly.dog>
2021-07-23 02:15:49 -0700

    notify

--- coq/treesitter/request.lua
+++ coq/treesitter/request.lua
@@ -1,14 +1,12 @@
 (function(...)
-  COQts_req = function(session, pos)
-     COQts_notify(session, {})
-  end
-
   local g1, ts_locals = pcall(require, "nvim-treesitter.locals")
   local g2, parsers = pcall(require, "nvim-treesitter.parsers")
   local g3, ts_utils = pcall(require, "nvim-treesitter.ts_utils")
 
   if not g1 or not g2 or not g3 then
-    return
+    COQts_req = function(session, pos)
+      COQts_notify(session, {})
+    end
   else
     local co = coroutine
 
