5aaa027fdb2a39630172f30598fba0fe6f0fbe61
DOG DOG DOG <github@bigly.dog>
2021-07-28 02:15:46 -0700

    save on work

--- coq/clients/lsp/worker.py
+++ coq/clients/lsp/worker.py
@@ -1,8 +1,10 @@
-from asyncio import as_completed
+from asyncio import as_completed, gather
+from enum import Enum, auto
 from itertools import chain
-from typing import AsyncIterator, Iterator, MutableSequence, Sequence
+from typing import AsyncIterator, Iterator, MutableSequence, Sequence, Tuple
 
 from std2.aitertools import anext
+from std2.asyncio import pure
 from std2.itertools import chunk
 
 from ...lsp.requests.completion import request
@@ -14,6 +16,12 @@ from ...shared.runtime import Worker as BaseWorker
 from ...shared.settings import BaseClient
 from ...shared.types import Completion, Context
 from ..cache.worker import CacheWorker
+
+
+class _Src(Enum):
+    dab = auto()
+    lit = auto()
+    lsp = auto()
 
 
 class Worker(BaseWorker[BaseClient, None], CacheWorker):
@@ -29,16 +37,16 @@ class Worker(BaseWorker[BaseClient, None], CacheWorker):
         if not use_cache:
             self._local_cached.clear()
 
-        async def cached_iters() -> LSPcomp:
+        async def cached_iters() -> Tuple[_Src, LSPcomp]:
             items = chain(*self._local_cached)
             self._local_cached.clear()
-            return LSPcomp(local_cache=True, items=items)
+            return _Src.lit, LSPcomp(local_cache=True, items=items)
 
-        async def cached_items() -> LSPcomp:
+        async def cached_items() -> Tuple[_Src, LSPcomp]:
             items = await cached
-            return LSPcomp(local_cache=False, items=items)
+            return _Src.dab, LSPcomp(local_cache=False, items=items)
 
-        async def stream() -> AsyncIterator[LSPcomp]:
+        async def stream() -> AsyncIterator[Tuple[_Src, LSPcomp]]:
             stream = request(
                 self._supervisor.nvim,
                 short_name=self._options.short_name,
@@ -50,46 +58,52 @@ class Worker(BaseWorker[BaseClient, None], CacheWorker):
                 (
                     cached_iters(),
                     cached_items(),
-                    anext(stream, LSPcomp(local_cache=False, items=iter(()))),
+                    gather(
+                        pure(_Src.lsp),
+                        anext(stream, LSPcomp(local_cache=False, items=iter(()))),
+                    ),
                 )
             ):
                 yield await fut
 
             async for lc in stream:
-                yield lc
+                yield _Src.lsp, lc
 
-        async for lsp_comps in stream():
+        async for src, lsp_comps in stream():
             if lsp_comps.local_cache:
                 self._local_cached.append(lsp_comps.items)
 
             for chunked in chunk(
                 lsp_comps.items, n=self._supervisor.options.max_results
             ):
+                if src is _Src.dab:
+                    yield chunked
+                else:
 
-                def cont() -> Iterator[Completion]:
-                    for c in chunked:
-                        cword = (
-                            w_before
-                            if is_word(
-                                c.sort_by[:1],
-                                unifying_chars=self._supervisor.options.unifying_chars,
+                    def cont() -> Iterator[Completion]:
+                        for c in chunked:
+                            cword = (
+                                w_before
+                                if is_word(
+                                    c.sort_by[:1],
+                                    unifying_chars=self._supervisor.options.unifying_chars,
+                                )
+                                else sw_before
                             )
-                            else sw_before
-                        )
-                        go = (
-                            quick_ratio(
-                                cword,
-                                lower(c.sort_by),
-                                look_ahead=self._supervisor.options.look_ahead,
+                            go = (
+                                quick_ratio(
+                                    cword,
+                                    lower(c.sort_by),
+                                    look_ahead=self._supervisor.options.look_ahead,
+                                )
+                                >= self._supervisor.options.fuzzy_cutoff
                             )
-                            >= self._supervisor.options.fuzzy_cutoff
-                        )
-                        if go:
-                            yield c
+                            if go:
+                                yield c
 
-                yield tuple(cont())
+                    yield tuple(cont())
 
-                if lsp_comps.local_cache and chunked:
-                    await set_cache(chunked)
-                    yield ()
+                    if lsp_comps.local_cache and chunked:
+                        await set_cache(chunked)
+                        yield ()
 
