71c7a4943a9d585612a579c03b385dfb04f76b20
DOG DOG DOG <github@bigly.dog>
2021-07-28 04:49:38 -0700

    br

--- coq/clients/paths/worker.py
+++ coq/clients/paths/worker.py
@@ -51,29 +51,34 @@ def parse(base: Path, line: str) -> Iterator[Tuple[str, str]]:
         sl, ss, sr = segment.rpartition(sep)
         sort_by = _p_lhs(sl) + ss + sr
 
-        e = Path(expandvars(Path(segment).expanduser()))
-        entire = e if e.is_absolute() else base / e
-        if entire.is_dir() and access(entire, mode=X_OK):
-            for path in entire.iterdir():
-                term = sep if path.is_dir() else ""
-                line = _join(segment, path.name) + term
-                yield line, sort_by
-            break
-        else:
-            lft, go, rhs = segment.rpartition(sep)
-            is_lower = lower(rhs) == rhs
-            assert go
-            lhs = lft + go
-            l = Path(lhs).expanduser()
-            left = l if l.is_absolute() else base / l
-            if left.is_dir() and access(left, mode=X_OK):
-                for path in left.iterdir():
-                    l_match = lower(path.name) if is_lower else normcase(path.name)
-                    if l_match.startswith(rhs):
-                        term = sep if path.is_dir() else ""
-                        line = _join(lhs, path.name) + term
-                        yield line, sort_by
-            break
+        p1 = Path(segment)
+        p2 = p1.expanduser()
+        p3 = Path(expandvars(p2))
+
+        for p in (p1, p2, p3):
+            entire = p if p.is_absolute() else base / p
+            if entire.is_dir() and access(entire, mode=X_OK):
+                for path in entire.iterdir():
+                    term = sep if path.is_dir() else ""
+                    line = _join(segment, path.name) + term
+                    yield line, sort_by
+                break
+
+            else:
+                lft, go, rhs = segment.rpartition(sep)
+                is_lower = lower(rhs) == rhs
+                assert go
+                lhs = lft + go
+                l = Path(lhs).expanduser()
+                left = l if l.is_absolute() else base / l
+                if left.is_dir() and access(left, mode=X_OK):
+                    for path in left.iterdir():
+                        l_match = lower(path.name) if is_lower else normcase(path.name)
+                        if l_match.startswith(rhs):
+                            term = sep if path.is_dir() else ""
+                            line = _join(lhs, path.name) + term
+                            yield line, sort_by
+                break
 
 
 async def _parse(base: Path, line: str, limit: int) -> AbstractSet[Tuple[str, str]]:
