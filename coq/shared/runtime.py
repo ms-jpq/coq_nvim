e533978ee1bd4b68434745931d449f5cc5032443
DOG DOG DOG <github@bigly.dog>
2021-07-27 20:07:44 -0700

    ok

--- coq/shared/runtime.py
+++ coq/shared/runtime.py
@@ -82,7 +82,6 @@ class Supervisor:
         self._lock = Lock()
         self._workers: MutableMapping[Worker, BaseClient] = WeakKeyDictionary()
 
-        self._task: Optional[Task] = None
         self._tasks: Sequence[Task] = ()
 
     @property
@@ -101,83 +100,71 @@ class Supervisor:
         go(self.nvim, aw=cont())
 
     def interrupt(self) -> None:
-        async def c1() -> None:
-            if self._task:
-                await cancel(self._task)
-                self._task = None
-
-        async def c2() -> None:
-            await cancel(gather(*self._tasks))
-            self._tasks = ()
-
         async def cont() -> None:
-            with timeit("CANCEL -- **ALL**", force=True):
-                await gather(c1(), c2())
+            with timeit("CANCEL -- **ALL**"):
+                await cancel(gather(*self._tasks))
+                self._tasks = ()
 
         go(self.nvim, aw=cont())
 
-    def collect(self, context: Context) -> Awaitable[Sequence[Metric]]:
-        async def cont() -> Sequence[Metric]:
-            with timeit("COLLECTED -- **ALL**"):
-                assert not self._lock.locked()
-                async with self._lock:
-                    done = False
-                    acc: MutableSequence[Metric] = []
-                    timeout = (
-                        self.limits.manual_timeout
-                        if context.manual
-                        else self.limits.timeout
+    async def collect(self, context: Context) -> Sequence[Metric]:
+        with timeit("COLLECTED -- **ALL**"):
+            assert not self._lock.locked()
+            async with self._lock:
+                done = False
+                acc: MutableSequence[Metric] = []
+                timeout = (
+                    self.limits.manual_timeout
+                    if context.manual
+                    else self.limits.timeout
+                )
+
+                async def supervise(worker: Worker, assoc: BaseClient) -> None:
+                    with timeit(f"WORKER -- {assoc.short_name}"):
+                        instance, t1 = uuid4(), monotonic()
+                        interrupted, items = True, 0
+                        await self._reviewer.s_begin(assoc, instance=instance)
+                        try:
+                            async for completions in worker.work(context):
+                                for comps in chunk(
+                                    completions, n=self.options.max_results
+                                ):
+                                    if not done:
+                                        metrics = self._reviewer.trans(
+                                            instance, completions=comps
+                                        )
+                                        acc.extend(metrics)
+                                    items += len(comps)
+                                    await sleep(0)
+                            else:
+                                interrupted = False
+                        finally:
+                            elapsed = monotonic() - t1
+                            await self._reviewer.s_end(
+                                instance,
+                                interrupted=interrupted,
+                                elapsed=elapsed,
+                                items=items,
+                            )
+
+                await self._reviewer.begin(context)
+                try:
+                    self._tasks = tuple(
+                        cast(Task, go(self.nvim, aw=supervise(worker, assoc=assoc)))
+                        for worker, assoc in self._workers.items()
                     )
-
-                    async def supervise(worker: Worker, assoc: BaseClient) -> None:
-                        with timeit(f"WORKER -- {assoc.short_name}"):
-                            instance, t1 = uuid4(), monotonic()
-                            interrupted, items = True, 0
-                            await self._reviewer.s_begin(assoc, instance=instance)
-                            try:
-                                async for completions in worker.work(context):
-                                    for comps in chunk(
-                                        completions, n=self.options.max_results
-                                    ):
-                                        if not done:
-                                            metrics = self._reviewer.trans(
-                                                instance, completions=comps
-                                            )
-                                            acc.extend(metrics)
-                                        items += len(comps)
-                                        await sleep(0)
-                                else:
-                                    interrupted = False
-                            finally:
-                                elapsed = monotonic() - t1
-                                await self._reviewer.s_end(
-                                    instance,
-                                    interrupted=interrupted,
-                                    elapsed=elapsed,
-                                    items=items,
-                                )
-
-                    await self._reviewer.begin(context)
-                    try:
-                        self._tasks = tuple(
-                            cast(Task, go(self.nvim, aw=supervise(worker, assoc=assoc)))
-                            for worker, assoc in self._workers.items()
-                        )
-                        if not self._tasks:
-                            return ()
-                        else:
-                            _, pending = await wait(self._tasks, timeout=timeout)
-                            if not acc:
-                                for fut in as_completed(pending):
-                                    await fut
-                                    if acc:
-                                        break
-                            return acc
-                    finally:
-                        done = True
-
-        self._task = cast(Task, go(self.nvim, aw=cont()))
-        return self._task
+                    if not self._tasks:
+                        return ()
+                    else:
+                        _, pending = await wait(self._tasks, timeout=timeout)
+                        if not acc:
+                            for fut in as_completed(pending):
+                                await fut
+                                if acc:
+                                    break
+                        return acc
+                finally:
+                    done = True
 
 
 class Worker(Generic[O_co, T_co]):
