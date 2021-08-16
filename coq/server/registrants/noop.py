8f399a8606ff585fde26cfb094f5fcb2141180cd
Fly me to the moon <git@bigly.dog>
2021-08-15 20:26:21 -0700

    added shut up

--- coq/server/registrants/noop.py
+++ coq/server/registrants/noop.py
@@ -1,9 +1,11 @@
+from argparse import Namespace
 from dataclasses import dataclass
 from itertools import chain
 from random import choice, sample
 from typing import Sequence, Tuple
 
 from pynvim import Nvim
+from std2.argparse import ArgParser
 from std2.pickle import new_decoder
 from yaml import safe_load
 
@@ -23,12 +25,20 @@ class _Helo:
 _HELO = new_decoder(_Helo)(safe_load(HELO_ARTIFACTS.read_text("UTF-8")))
 
 
+def _parse_args(args: Sequence[str]) -> Namespace:
+    parser = ArgParser()
+    parser.add_argument("-s", "--shut-up", action="store_true")
+    return parser.parse_args(args)
+
+
 @rpc(blocking=True)
-def now(nvim: Nvim, stack: Stack, *_: str) -> None:
-    lo, hi = _HELO.chars
-    chars = choice(range(lo, hi))
-    star = (choice(_HELO.stars),)
-    birds = " ".join(chain(star, sample(_HELO.cocks, k=chars), star))
-    helo = choice(_HELO.helo)
-    msg = f"{birds}  {helo}"
-    print(msg, flush=True)
+def now(nvim: Nvim, stack: Stack, args: Sequence[str]) -> None:
+    ns = _parse_args(args)
+    if not ns.stfu:
+        lo, hi = _HELO.chars
+        chars = choice(range(lo, hi))
+        star = (choice(_HELO.stars),)
+        birds = " ".join(chain(star, sample(_HELO.cocks, k=chars), star))
+        helo = choice(_HELO.helo)
+        msg = f"{birds}  {helo}"
+        print(msg, flush=True)
