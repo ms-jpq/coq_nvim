b014bf2041e5234afab19d72b751b88a5abb828c
Fly me to the moon <git@bigly.dog>
2021-08-23 21:41:18 -0700

    no win up

--- coq/server/nvim/completions.py
+++ coq/server/nvim/completions.py
@@ -50,8 +50,10 @@ _LUA = """
     if mode == "i" or mode == "ic" or mode == "ix" then
       if #items ~= 0 then
         vim.fn.complete(col, items)
-      elseif mode == "ic" or mode == "ix" then
+      elseif mode == "ic" then
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-e>", true, true, true), "n", true)
+      else
+        vim.fn.complete(col, items)
       end
     end
   end)
