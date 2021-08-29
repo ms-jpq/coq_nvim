184491fda570ef8612818d027e65bbabc300630e
Fly me to the moon <git@bigly.dog>
2021-08-28 22:06:26 -0700

    use built-in get text

--- coq/treesitter/request.lua
+++ coq/treesitter/request.lua
@@ -33,30 +33,16 @@
     )
   end
 
-  local text_for = function(node)
-    local r1, c1, r2, c2 = node:range()
-    local lines = vim.api.nvim_buf_get_lines(0, r1, r2 + 1, false)
-    local len = #lines
-    if len == 1 then
-      local word = unpack(lines)
-      return string.sub(word, c1 + 1, c2)
-    else
-      local head, tail = unpack({lines[1], lines[len]})
-      local pre = string.sub(head, c1 + 1, #head)
-      local post = string.sub(tail, 1, c2)
-      lines[1], lines[len] = unpack({pre, post})
-      local word = table.concat(lines, "\n")
-      return word
-    end
-  end
-
   COQts_req = function(session, pos)
     vim.schedule(
       function()
         local acc = {}
         for node in iter_nodes() do
           if not node:missing() and not node:has_error() then
-            table.insert(acc, {text = text_for(node), kind = node:type()})
+            table.insert(
+              acc,
+              {text = vim.treesitter.get_node_text(node), kind = node:type()}
+            )
           end
         end
         COQts_notify(session, acc)
