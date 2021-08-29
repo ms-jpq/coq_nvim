10973871068645d4395dbb257a86a069ad60c7cf
Fly me to the moon <git@bigly.dog>
2021-08-29 11:07:05 -0700

    tree sitter gp

--- coq/clients/tree_sitter/worker.py
+++ coq/clients/tree_sitter/worker.py
@@ -1,4 +1,5 @@
-from typing import AsyncIterator
+from os import linesep
+from typing import AsyncIterator, Iterator, Optional
 
 from pynvim_pp.lib import go
 
@@ -6,11 +7,30 @@ from ...databases.treesitter.database import TDB
 from ...shared.runtime import Supervisor
 from ...shared.runtime import Worker as BaseWorker
 from ...shared.settings import BaseClient
-from ...shared.types import Completion, Context, Edit
+from ...shared.types import Completion, Context, Doc, Edit
 from ...treesitter.types import Payload
 
 
-def _trans(client: BaseClient, payload: Payload) -> Completion:
+def _doc(syntax: str, payload: Payload) -> Optional[Doc]:
+    def cont() -> Iterator[str]:
+        if payload.grandparent:
+            yield payload.grandparent.kind
+            yield " -> "
+            yield payload.grandparent.text
+
+        if payload.grandparent and payload.parent:
+            yield linesep
+
+        if payload.parent:
+            yield payload.parent.kind
+            yield " -> "
+            yield payload.parent.text
+
+    doc = Doc(syntax=syntax, text="".join(cont()))
+    return doc
+
+
+def _trans(client: BaseClient, syntax: str, payload: Payload) -> Completion:
     edit = Edit(new_text=payload.text)
     cmp = Completion(
         source=client.short_name,
@@ -19,6 +39,7 @@ def _trans(client: BaseClient, payload: Payload) -> Completion:
         sort_by=payload.text,
         primary_edit=edit,
         kind=payload.kind,
+        doc=_doc(syntax, payload=payload),
     )
     return cmp
 
@@ -42,4 +63,4 @@ class Worker(BaseWorker[BaseClient, TDB]):
         )
 
         for payload in payloads:
-            yield _trans(self._options, payload=payload)
+            yield _trans(self._options, syntax=context.filetype, payload=payload)
