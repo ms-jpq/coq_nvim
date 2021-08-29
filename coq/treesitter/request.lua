a9dcd42ed10bccfcaddaeb701150e6dd8e079825
Fly me to the moon <git@bigly.dog>
2021-08-28 20:42:55 -0700

    move recur

--- coq/treesitter/request.lua
+++ coq/treesitter/request.lua
@@ -1,27 +1,27 @@
 (function(...)
+  local iter = nil
+  iter = function(root)
+    return coroutine.wrap(
+      function()
+        if root:child_count() == 0 then
+          coroutine.yield(root)
+        else
+          for node in root:iter_children() do
+            for n in iter(node) do
+              coroutine.yield(n)
+            end
+          end
+        end
+      end
+    )
+  end
+
   local iter_nodes = function()
     return coroutine.wrap(
       function()
         local go, parser = pcall(vim.treesitter.get_parser)
         if go then
           local trees = parser:parse()
-
-          local iter = nil
-          iter = function(root)
-            return coroutine.wrap(
-              function()
-                if root:child_count() == 0 then
-                  coroutine.yield(root)
-                else
-                  for node in root:iter_children() do
-                    for n in iter(node) do
-                      coroutine.yield(n)
-                    end
-                  end
-                end
-              end
-            )
-          end
 
           for _, tree in ipairs(trees) do
             for node in iter(tree:root()) do
