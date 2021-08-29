c8e5d8a0423f1c29c17105ea9032491f33595f46
Fly me to the moon <git@bigly.dog>
2021-08-29 12:28:47 -0700

    mv

--- coq/lsp/requests/completion.lua
+++ coq/lsp/requests/completion.lua
@@ -1,17 +1,17 @@
 (function(...)
   local cancel, cur_session = nil, nil
 
-  local on_resp_old = function(err, _, resp, client_id)
-    if session_id == cur_session then
-      n_clients = n_clients - 1
-      COQlsp_notify(name, session_id, n_clients == 0, resp or vim.NIL)
-    end
-  end
-
-  local on_resp = on_resp_old
-
   COQlsp_comp = function(name, session_id, pos)
     cur_session = session_id
+
+    local on_resp_old = function(err, _, resp, client_id)
+      if session_id == cur_session then
+        n_clients = n_clients - 1
+        COQlsp_notify(name, session_id, n_clients == 0, resp or vim.NIL)
+      end
+    end
+
+    local on_resp = on_resp_old
 
     if cancel then
       pcall(cancel)
