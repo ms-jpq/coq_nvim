a8d3e34c695f5f30966a0899419cf762bf415e6d
Fly me to the moon <git@bigly.dog>
2021-08-30 02:29:28 -0700

    use replace

--- coq/server/icons.py
+++ coq/server/icons.py
@@ -1,3 +1,5 @@
+from dataclasses import replace
+
 from ..shared.settings import Icons
 from ..shared.types import Completion
 
@@ -7,4 +9,8 @@ def iconify(icons: Icons, completion: Completion) -> Completion:
         return completion
     else:
         alias = icons.alias.get(completion.icon_match, completion.icon_match)
-        return completion
+        kind = icons.mappings.get(alias)
+        if not kind:
+            return completion
+        else:
+            return replace(completion, kind=kind)
