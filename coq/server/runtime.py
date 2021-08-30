6ae82d69dfa4a657e8be744345e8265adf50d07b
Fly me to the moon <git@bigly.dog>
2021-08-30 00:45:34 -0700

    path mod

--- coq/server/runtime.py
+++ coq/server/runtime.py
@@ -84,7 +84,7 @@ def _from_each_according_to_their_ability(
 
 def stack(pool: Executor, nvim: Nvim) -> Stack:
     settings = _settings(nvim)
-    vars_dir = Path(nvim.api.stdpath("cache")) if settings.xdg else VARS
+    vars_dir = Path(nvim.funcs.stdpath("cache")) / "coq" if settings.xdg else VARS
     s = state(cwd=PurePath(get_cwd(nvim)))
     bdb, sdb, idb, tdb, ctdb, tmdb = (
         BDB(pool),
