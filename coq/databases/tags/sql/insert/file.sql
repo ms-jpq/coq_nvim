a3d4f8daee90706fd84f459ef078b6e476b28b98
Fly me to the moon <git@bigly.dog>
2021-08-31 01:27:27 -0700

    normcase

--- coq/databases/tags/sql/insert/file.sql
+++ coq/databases/tags/sql/insert/file.sql
@@ -1,3 +1,2 @@
-INSERT INTO files ( filename,  filetype,  mtime)
-VALUES            (:filename, :filetype, :mtime)
-
+INSERT INTO files (filename,                filetype,  mtime)
+VALUES            (X_NORM_CASE(:filename), :filetype, :mtime)
