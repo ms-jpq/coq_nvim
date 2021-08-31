f5462ece24519851998d4446f772e1ce75ae783c
Fly me to the moon <git@bigly.dog>
2021-08-31 00:47:00 -0700

    t9 worker

--- coq/clients/t9/worker.py
+++ coq/clients/t9/worker.py
@@ -8,13 +8,14 @@ from json.decoder import JSONDecodeError
 from os import X_OK, access, linesep
 from pathlib import PurePath
 from subprocess import DEVNULL, PIPE
-from typing import Any, AsyncIterator, Iterator, Optional
+from typing import AbstractSet, Any, AsyncIterator, Iterator, Optional
 
 from pynvim_pp.lib import awrite, go
 from pynvim_pp.logging import log
 from std2.pickle import DecodeError, new_decoder, new_encoder
 
 from ...lang import LANG
+from ...shared.parse import is_word
 from ...shared.runtime import Supervisor
 from ...shared.runtime import Worker as BaseWorker
 from ...shared.settings import BaseClient, Options
@@ -48,7 +49,26 @@ def _encode(options: Options, context: Context, limit: int) -> Any:
     return _ENCODER(req)
 
 
-def _decode(client: BaseClient, reply: Any) -> Iterator[Completion]:
+def _sort_by(unifying_chars: AbstractSet[str], new_text: str) -> str:
+    def cont() -> Iterator[str]:
+        seen_syms = False
+        for char in reversed(new_text):
+            if is_word(char, unifying_chars=unifying_chars):
+                if seen_syms:
+                    break
+                else:
+                    yield char
+            else:
+                yield char
+                seen_syms = True
+
+    sort_by = "".join(reversed(tuple(cont())))
+    return sort_by
+
+
+def _decode(
+    unifying_chars: AbstractSet[str], client: BaseClient, reply: Any
+) -> Iterator[Completion]:
     try:
         resp: Response = _DECODER(reply)
     except DecodeError as e:
@@ -68,7 +88,7 @@ def _decode(client: BaseClient, reply: Any) -> Iterator[Completion]:
                 source=client.short_name,
                 weight_adjust=client.weight_adjust,
                 label=label,
-                sort_by=edit.new_text,
+                sort_by=_sort_by(unifying_chars, new_text=edit.old_prefix),
                 primary_edit=edit,
                 icon_match=None,
             )
@@ -183,5 +203,9 @@ class Worker(BaseWorker[BaseClient, None]):
                 except JSONDecodeError as e:
                     log.warn("%s", e)
                 else:
-                    for comp in _decode(self._options, reply=resp):
+                    for comp in _decode(
+                        self._supervisor.options.unifying_chars,
+                        client=self._options,
+                        reply=resp,
+                    ):
                         yield comp
