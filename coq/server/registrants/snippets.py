f18e953b7f1eb3c924fe4e52b3d3cfac72905a11
Fly me to the moon <git@bigly.dog>
2021-09-01 21:47:49 -0700

    snip parse

--- coq/server/registrants/snippets.py
+++ coq/server/registrants/snippets.py
@@ -1,14 +1,19 @@
+from dataclasses import dataclass
+from locale import strxfrm
 from pathlib import PurePath
-from typing import Iterator, Sequence, Tuple
+from typing import Iterator, Mapping, Sequence
 
 from pynvim.api.nvim import Nvim
-from pynvim_pp.api import buf_get_lines, buf_line_count, cur_buf
+from pynvim_pp.api import buf_get_lines, buf_line_count, cur_win, win_get_buf
+from pynvim_pp.hold import hold_win_pos
 from pynvim_pp.lib import write
 from pynvim_pp.operators import operator_marks
 from pynvim_pp.preview import set_preview
+from std2.itertools import group_by
 
 from ...registry import rpc
-from ...shared.types import Edit, Mark, SnippetEdit
+from ...shared.types import SnippetEdit
+from ...snippets.consts import MOD_PAD
 from ...snippets.loaders.neosnippet import parse as parse_neosnippets
 from ...snippets.parse import parse
 from ...snippets.parsers.types import ParseError
@@ -18,9 +23,17 @@ from ..rt_types import Stack
 from ..state import state
 
 
+@dataclass(frozen=True)
+class _Snip:
+    matches: Sequence[str]
+    parsed: str
+    marks: Mapping[int, Sequence[str]]
+
+
 @rpc(blocking=True)
 def eval_snips(nvim: Nvim, stack: Stack, visual: bool) -> None:
-    buf = cur_buf(nvim)
+    win = cur_win(nvim)
+    buf = win_get_buf(nvim, win=win)
     line_count = buf_line_count(nvim, buf=buf)
 
     if visual:
@@ -40,31 +53,44 @@ def eval_snips(nvim: Nvim, stack: Stack, visual: bool) -> None:
     lines = buf_get_lines(nvim, buf=buf, lo=lo, hi=hi)
 
     try:
-        exts, snips = parse_neosnippets(path, lines=enumerate(lines, start=lo + 1))
+        _, snips = parse_neosnippets(path, lines=enumerate(lines, start=lo + 1))
     except LoadError as e:
         preview = str(e).splitlines()
-        set_preview(nvim, syntax="", preview=preview)
+        with hold_win_pos(nvim, win=win):
+            set_preview(nvim, syntax="", preview=preview)
         write(nvim, "snip load fail")
     else:
 
-        def cont() -> Iterator[Tuple[Edit, Sequence[Mark]]]:
+        def cont() -> Iterator[_Snip]:
             for snip in snips:
                 edit = SnippetEdit(grammar="lsp", new_text=snip.content)
-                parsed = parse(
+                parsed, marks = parse(
                     stack.settings.match.unifying_chars,
                     context=ctx,
                     snippet=edit,
                     visual="",
                 )
-                yield parsed
+                snippet = _Snip(
+                    matches=sorted(snip.matches, key=strxfrm),
+                    parsed=parsed.new_text,
+                    marks=group_by(
+                        marks,
+                        key=lambda m: m.idx % MOD_PAD,
+                        val=lambda m: m.text,
+                    ),
+                )
+                yield snippet
 
         try:
             parsed = tuple(cont())
         except ParseError as e:
             preview = str(e).splitlines()
-            set_preview(nvim, syntax="", preview=preview)
+
+            with hold_win_pos(nvim, win=win):
+                set_preview(nvim, syntax="", preview=preview)
             write(nvim, "snip parse fail")
         else:
             preview = str(parsed).splitlines()
-            set_preview(nvim, syntax="", preview=preview)
+            with hold_win_pos(nvim, win=win):
+                set_preview(nvim, syntax="", preview=preview)
             write(nvim, "snip parse succ")
