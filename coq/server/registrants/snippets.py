d517adfed7a437b7971b3d4a4f2bdb2657fb97b3
Fly me to the moon <git@bigly.dog>
2021-09-01 21:33:59 -0700

    snips

--- coq/server/registrants/snippets.py
+++ coq/server/registrants/snippets.py
@@ -1,4 +1,5 @@
-from os import linesep
+from pathlib import PurePath
+from typing import Iterator, Sequence, Tuple
 
 from pynvim.api.nvim import Nvim
 from pynvim_pp.api import buf_get_lines, buf_line_count, cur_buf
@@ -7,9 +8,11 @@ from pynvim_pp.operators import operator_marks
 from pynvim_pp.preview import set_preview
 
 from ...registry import rpc
-from ...shared.types import SnippetEdit
+from ...shared.types import Edit, Mark, SnippetEdit
+from ...snippets.loaders.neosnippet import parse as parse_neosnippets
 from ...snippets.parse import parse
 from ...snippets.parsers.types import ParseError
+from ...snippets.types import LoadError
 from ..context import context
 from ..rt_types import Stack
 from ..state import state
@@ -33,21 +36,35 @@ def eval_snips(nvim: Nvim, stack: Stack, visual: bool) -> None:
         state=state(),
         manual=True,
     )
+    path = PurePath(ctx.filename)
     lines = buf_get_lines(nvim, buf=buf, lo=lo, hi=hi)
-    snip = SnippetEdit(grammar="lsp", new_text=linesep.join(lines))
 
     try:
-        parsed = parse(
-            stack.settings.match.unifying_chars,
-            context=ctx,
-            snippet=snip,
-            visual="",
-        )
-    except ParseError as e:
+        exts, snips = parse_neosnippets(path, lines=enumerate(lines, start=lo + 1))
+    except LoadError as e:
         preview = str(e).splitlines()
         set_preview(nvim, syntax="", preview=preview)
-        write(nvim, "snip parse fail")
+        write(nvim, "snip load fail")
     else:
-        preview = str(parsed).splitlines()
-        set_preview(nvim, syntax="", preview=preview)
-        write(nvim, "snip parse succ")
+
+        def cont() -> Iterator[Tuple[Edit, Sequence[Mark]]]:
+            for snip in snips:
+                edit = SnippetEdit(grammar="lsp", new_text=snip.content)
+                parsed = parse(
+                    stack.settings.match.unifying_chars,
+                    context=ctx,
+                    snippet=edit,
+                    visual="",
+                )
+                yield parsed
+
+        try:
+            parsed = tuple(cont())
+        except ParseError as e:
+            preview = str(e).splitlines()
+            set_preview(nvim, syntax="", preview=preview)
+            write(nvim, "snip parse fail")
+        else:
+            preview = str(parsed).splitlines()
+            set_preview(nvim, syntax="", preview=preview)
+            write(nvim, "snip parse succ")
