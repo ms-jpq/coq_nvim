3a118aecfefcc06defa3588c6ef22fe92e2eb090
Fly me to the moon <git@bigly.dog>
2021-09-03 12:55:08 -0700

    ok

--- coq/databases/snippets/sql/select/snippets.sql
+++ coq/databases/snippets/sql/select/snippets.sql
@@ -9,6 +9,11 @@ LEFT JOIN enabled_sources
 ON
   enabled_sources.source = snippets_view.source_id
 WHERE
+  CASE
+    WHEN NOT (SELECT COUNT(*) FROM enabled_sources) THEN 1
+    ELSE enabled_sources.source <> NULL
+  END
+  AND
   :word <> ''
   AND 
   LENGTH(prefix) + :look_ahead >= LENGTH(:word)
@@ -20,9 +25,4 @@ WHERE
   X_SIMILARITY(LOWER(:word), lprefix, :look_ahead) > :cut_off
 GROUP BY
   snippet_id
-HAVING
-  CASE
-    WHEN NOT (SELECT COUNT(*) FROM enabled_sources) THEN 1
-    ELSE enabled_sources.source <> NULL
-  END
 LIMIT :limit
