29dc704b5de54ccfd5109716976a5ea1e06a4275
Fly me to the moon <git@bigly.dog>
2021-09-06 00:54:28 -0700

    udiff

--- coq/ci/main.py
+++ coq/ci/main.py
@@ -1,4 +1,5 @@
 from asyncio.tasks import gather
+from difflib import unified_diff
 
 from ..consts import LSP_ARTIFACTS, VARS
 from ..server.registrants.snippets import BUNDLED_PATH_TPL, jsonify
@@ -9,13 +10,16 @@ from .lsp import lsp
 
 async def main() -> None:
     lsprotocol, snippets = await gather(lsp(), load_parsable())
+    j_lsp, j_snippets = jsonify(lsprotocol), jsonify(snippets)
 
-    j_lsp = jsonify(lsprotocol)
-    LSP_ARTIFACTS.write_text(j_lsp)
-
-    j_snippets = jsonify(snippets)
-    bj_snippets = j_snippets.encode()
-
+    LSP_ARTIFACTS.write_text(j_lsp, encoding="UTF-8")
     snip_art = VARS / "snippets" / BUNDLED_PATH_TPL.substitute(schema=SCHEMA)
     snip_art.parent.mkdir(parents=True, exist_ok=True)
-    snip_art.write_bytes(bj_snippets)
+
+    if snip_art.exists():
+        for line in unified_diff(
+            snip_art.read_text().splitlines(), j_snippets.splitlines()
+        ):
+            print(line)
+
+    snip_art.write_text(j_snippets, encoding="UTF-8")
