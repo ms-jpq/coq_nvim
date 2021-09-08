94403f3370913cc2abee5949da39160b6a082028
Fly me to the moon <git@bigly.dog>
2021-09-08 10:39:53 -0700

    comp

--- coq/lsp/requests/completion.py
+++ coq/lsp/requests/completion.py
@@ -1,8 +1,11 @@
+from collections.abc import Sized
 from pathlib import Path
 from typing import AsyncIterator, cast
 
 from pynvim.api.nvim import Nvim
+from pynvim_pp.logging import log
 
+from ...consts import DEBUG
 from ...registry import atomic
 from ...shared.types import UTF16, Context
 from ..parse import parse
@@ -25,4 +28,8 @@ async def request(
 
     async for client, reply in async_request(nvim, "COQlsp_comp", (row, col)):
         resp = cast(CompletionResponse, reply)
-        yield parse(short_name, weight_adjust=weight_adjust,  resp=resp)
+        if DEBUG:
+            thing = len(resp) if isinstance(resp, Sized) else resp
+            msg = f"{client} {thing}"
+            log.info("%s", msg)
+        yield parse(short_name, weight_adjust=weight_adjust, resp=resp)
