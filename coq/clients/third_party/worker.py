e5508674d38985c3d2778727be393fbd22513740
Fly me to the moon <git@bigly.dog>
2021-09-12 11:25:26 -0700

    third party driver begin

---
+++ coq/clients/third_party/worker.py
@@ -0,0 +1,13 @@
+from typing import AsyncIterator
+
+from std2.aitertools import to_async
+
+from ...lsp.types import LSPcomp
+from ...shared.types import Context
+from ..lsp.worker import Worker as LSPWorker
+
+
+class Worker(LSPWorker):
+    def _request(self, context: Context) -> AsyncIterator[LSPcomp]:
+        print(context, flush=True)
+        return to_async(())
