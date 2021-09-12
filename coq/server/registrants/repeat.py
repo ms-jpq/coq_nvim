5e2e981f29fbd6e9321361d1e5f12869ca762a12
Fly me to the moon <git@bigly.dog>
2021-09-11 21:44:50 -0700

    ok

--- coq/server/registrants/repeat.py
+++ coq/server/registrants/repeat.py
@@ -3,26 +3,23 @@ from uuid import uuid4
 from pynvim.api.nvim import Nvim
 
 from ...registry import rpc
-from ...shared.types import Edit
+from ...shared.repeat import sanitize
 from ..edit import edit
 from ..nvim.completions import UserData
 from ..runtime import Stack
 from ..state import state
 
 
-def _edit(prev: Edit) -> Edit:
-    return prev
-
-
 @rpc(blocking=True)
 def repeat(nvim: Nvim, stack: Stack) -> None:
     s = state()
+    sanitized = sanitize(s.last_edit)
     data = UserData(
         uid=uuid4(),
         instance=uuid4(),
         sort_by="",
         change_uid=uuid4(),
-        primary_edit=_edit(s.last_edit),
+        primary_edit=sanitized,
         secondary_edits=(),
         doc=None,
         extern=None,
