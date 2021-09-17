3a938a67c45ad447b32ee774a62125d0e731e0f6
Fly me to the moon <git@bigly.dog>
2021-09-17 05:49:17 -0700

    ok

--- coq/databases/snippets/sql/select/snippets.sql
+++ coq/databases/snippets/sql/select/snippets.sql
@@ -8,31 +8,15 @@ FROM snippets_view
 WHERE
   snippet <> ''
   AND
-  (
-    (
-      :word <> ''
-      AND 
-      LENGTH(prefix) + :look_ahead >= LENGTH(:word)
-      AND
-      ft_src = :filetype
-      AND
-      lprefix LIKE X_LIKE_ESC(SUBSTR(LOWER(:word), 1, :exact)) ESCAPE '!'
-      AND
-      X_SIMILARITY(LOWER(:word), lprefix, :look_ahead) > :cut_off
-    )
-    OR
-    (
-      :sym <> ''
-      AND 
-      LENGTH(prefix) + :look_ahead >= LENGTH(:sym)
-      AND
-      ft_src = :filetype
-      AND
-      lprefix LIKE X_LIKE_ESC(SUBSTR(LOWER(:sym), 1, :exact)) ESCAPE '!'
-      AND
-      X_SIMILARITY(LOWER(:sym), lprefix, :look_ahead) > :cut_off
-    )
-  )
+  X_PICK_WS(word, :word, :sym) <> ''
+  AND 
+  LENGTH(prefix) + :look_ahead >= LENGTH(X_PICK_WS(word, :word, :sym))
+  AND
+  ft_src = :filetype
+  AND
+  lprefix LIKE X_LIKE_ESC(SUBSTR(LOWER(X_PICK_WS(word, :word, :sym)), 1, :exact)) ESCAPE '!'
+  AND
+  X_SIMILARITY(LOWER(X_PICK_WS(word, :word, :sym)), lprefix, :look_ahead) > :cut_off
 GROUP BY
   snippet_id
 LIMIT :limit
--- coq/databases/tags/sql/select/tags.sql
+++ coq/databases/tags/sql/select/tags.sql
@@ -15,33 +15,15 @@ ON files.filename = tags.`path`
 WHERE
   tags.name <> ''
   AND
-  (
-    (
-      :word <> ''
-      AND 
-      LENGTH(tags.name) + :look_ahead >= LENGTH(:word)
-      AND
-      files.filetype = :filetype
-      AND
-      tags.lname LIKE X_LIKE_ESC(SUBSTR(LOWER(:word), 1, :exact)) ESCAPE '!'
-      AND
-      NOT INSTR(:word, tags.name)
-      AND
-      X_SIMILARITY(LOWER(:word), tags.lname, :look_ahead) > :cut_off
-    )
-    OR
-    (
-      :sym <> ''
-      AND 
-      LENGTH(tags.name) + :look_ahead >= LENGTH(:sym)
-      AND
-      files.filetype = :filetype
-      AND
-      tags.lname LIKE X_LIKE_ESC(SUBSTR(LOWER(:sym), 1, :exact)) ESCAPE '!'
-      AND
-      NOT INSTR(:sym, tags.name)
-      AND
-      X_SIMILARITY(LOWER(:sym), tags.lname, :look_ahead) > :cut_off
-    )
-  )
+  X_PICK_WS(word, :word, :sym) <> ''
+  AND 
+  LENGTH(tags.name) + :look_ahead >= LENGTH(X_PICK_WS(word, :word, :sym))
+  AND
+  files.filetype = :filetype
+  AND
+  tags.lname LIKE X_LIKE_ESC(SUBSTR(LOWER(X_PICK_WS(word, :word, :sym)), 1, :exact)) ESCAPE '!'
+  AND
+  NOT INSTR(X_PICK_WS(word, :word, :sym), tags.name)
+  AND
+  X_SIMILARITY(LOWER(X_PICK_WS(word, :word, :sym)), tags.lname, :look_ahead) > :cut_off
 LIMIT :limit
