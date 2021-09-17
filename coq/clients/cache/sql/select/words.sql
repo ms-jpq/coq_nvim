93a5251090f9c5372ec125139eb5ab2c05954174
Fly me to the moon <git@bigly.dog>
2021-09-17 05:45:54 -0700

    pickws

--- coq/clients/cache/sql/select/words.sql
+++ coq/clients/cache/sql/select/words.sql
@@ -4,29 +4,14 @@ FROM words
 WHERE
   word <> ''
   AND
-  (
-    (
-      :word <> ''
-      AND 
-      LENGTH(word) + :look_ahead >= LENGTH(:word)
-      AND
-      lword LIKE X_LIKE_ESC(SUBSTR(LOWER(:word), 1, :exact)) ESCAPE '!'
-      AND
-      NOT INSTR(:word, word)
-      AND
-      X_SIMILARITY(LOWER(:word), lword, :look_ahead) > :cut_off
-    )
-  OR
-    (
-      :sym <> ''
-      AND 
-      LENGTH(word) + :look_ahead >= LENGTH(:sym)
-      AND
-      lword LIKE X_LIKE_ESC(SUBSTR(LOWER(:sym), 1, :exact)) ESCAPE '!'
-      AND
-      NOT INSTR(:sym, word)
-      AND
-      X_SIMILARITY(LOWER(:sym), lword, :look_ahead) > :cut_off
-    )
-  )
+  X_PICK_WS(word, :word, :sym) <> ''
+  AND 
+  LENGTH(word) + :look_ahead >= LENGTH(X_PICK_WS(word, :word, :sym))
+  AND
+  lword LIKE X_LIKE_ESC(SUBSTR(LOWER(X_PICK_WS(word, :word, :sym)), 1, :exact)) ESCAPE '!'
+  AND
+  NOT INSTR(X_PICK_WS(word, :word, :sym), word)
+  AND
+  X_SIMILARITY(LOWER(X_PICK_WS(word, :word, :sym)), lword, :look_ahead) > :cut_off
+
 LIMIT :limit
