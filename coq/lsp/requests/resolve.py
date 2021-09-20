e8175f71e37ed940adf133dd11f8ff614510eae6
Fly me to the moon <git@bigly.dog>
2021-09-20 09:52:12 -0700

    fix

--- coq/lsp/requests/resolve.py
+++ coq/lsp/requests/resolve.py
@@ -9,11 +9,12 @@ from .request import async_request
 
 
 async def resolve(nvim: Nvim, extern: ExternLSP) -> Optional[Completion]:
-    name = "lsp_resolve" if isinstance(extern, ExternLUA) else "lsp_third_party_resolve"
+    name = "lsp_third_party_resolve" if isinstance(extern, ExternLUA) else "lsp_resolve"
 
     async for _, resp in async_request(nvim, name, extern.item):
-        comp = parse_item(type(extern), short_name="", weight_adjust=0, item=resp)
-        if comp and comp.doc:
-            return comp
+        if resp:
+            comp = parse_item(type(extern), short_name="", weight_adjust=0, item=resp)
+            if comp and comp.doc:
+                return comp
     else:
         return None
