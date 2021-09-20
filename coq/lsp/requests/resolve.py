4ac7b7c43fd17eecc1318fb0c5101a1bcdcb8003
Fly me to the moon <git@bigly.dog>
2021-09-20 00:59:10 -0700

    ok

--- coq/lsp/requests/resolve.py
+++ coq/lsp/requests/resolve.py
@@ -11,7 +11,9 @@ from .request import async_request
 async def resolve(
     nvim: Nvim, extern_type: Type[ExternLSP], item: Mapping
 ) -> Optional[Completion]:
-    if extern_type is ExternLSP:
+    if extern_type is not ExternLSP:
+        return None
+    else:
         stream = async_request(nvim, "lsp_resolve", item)
         async for _, resp in stream:
             comp = parse_item(ExternLSP, short_name="", weight_adjust=0, item=resp)
