3fceb30360e47319b48590669505bc0b8db98fe1
Fly me to the moon <git@bigly.dog>
2021-09-22 23:12:12 -0700

    faster sigmoid

--- coq/server/reviewer.py
+++ coq/server/reviewer.py
@@ -1,7 +1,6 @@
 from collections import Counter
 from dataclasses import dataclass
 from itertools import chain
-from math import atan, e, pi
 from typing import Mapping
 from uuid import UUID, uuid4
 
@@ -44,7 +43,7 @@ def sigmoid(x: float) -> float:
     x -> y ∈ (0.5, 1.5)
     """
 
-    return atan(x) / pi + 1
+    return x / (1 + abs(x)) / 2 + 1
 
 
 def _join(
