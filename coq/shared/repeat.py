793c259a2698c1675eddf9085a67af0ab7ff6abd
Fly me to the moon <git@bigly.dog>
2021-09-26 21:23:34 -0700

    smarter cache

--- coq/shared/repeat.py
+++ coq/shared/repeat.py
@@ -1,9 +1,15 @@
-from .types import Edit, RangeEdit, SnippetEdit
+from .types import Edit, RangeEdit, SnippetEdit, SnippetRangeEdit
 
 
 def sanitize(edit: Edit) -> Edit:
-    if isinstance(edit, SnippetEdit):
-        return SnippetEdit(grammar=edit.grammar, new_text=edit.new_text)
+    if isinstance(edit, SnippetRangeEdit):
+        return (
+            SnippetEdit(grammar=edit.grammar, new_text=edit.new_text)
+            if edit.fallback == edit.new_text
+            else Edit(new_text=edit.fallback)
+        )
+    elif isinstance(edit, SnippetEdit):
+        return edit
     elif isinstance(edit, RangeEdit):
         return Edit(new_text=edit.fallback)
     else:
