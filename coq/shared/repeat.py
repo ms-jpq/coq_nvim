c71e8388450bbbd128e3d4b590c3658e9eb276b6
Fly me to the moon <git@bigly.dog>
2021-09-26 21:18:57 -0700

    cached snippets are still snippets

--- coq/shared/repeat.py
+++ coq/shared/repeat.py
@@ -2,9 +2,9 @@ from .types import Edit, RangeEdit, SnippetEdit
 
 
 def sanitize(edit: Edit) -> Edit:
-    if isinstance(edit, RangeEdit):
+    if isinstance(edit, SnippetEdit):
+        return SnippetEdit(grammar=edit.grammar, new_text=edit.new_text)
+    elif isinstance(edit, RangeEdit):
         return Edit(new_text=edit.fallback)
-    elif isinstance(edit, SnippetEdit):
-        return edit
     else:
         return Edit(new_text=edit.new_text)
