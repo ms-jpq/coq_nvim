ba58af8e9fd1f61664a3e940458a9151c0b9e819
Fly me to the moon <git@bigly.dog>
2021-09-28 12:03:07 -0700

    dc

--- coq/databases/snippets/sql/select/snippets.sql
+++ coq/databases/snippets/sql/select/snippets.sql
@@ -20,8 +20,6 @@ WHERE
       AND 
       LENGTH(word) + :look_ahead >= LENGTH(:word)
       AND
-      NOT INSTR(:word, word)
-      AND
       X_SIMILARITY(LOWER(:word), lword, :look_ahead) > :cut_off
     )
     OR
@@ -31,8 +29,6 @@ WHERE
       lword LIKE :like_sym ESCAPE '!'
       AND 
       LENGTH(word) + :look_ahead >= LENGTH(:sym)
-      AND
-      NOT INSTR(:sym, word)
       AND
       X_SIMILARITY(LOWER(:sym), lword, :look_ahead) > :cut_off
     )
