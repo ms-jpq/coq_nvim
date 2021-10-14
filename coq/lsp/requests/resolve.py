fe9e00e4d293c0a215c4c0ee05d02c7895da2236
Fly me to the moon <git@bigly.dog>
2021-10-13 22:50:40 -0700

    resolve comps

--- coq/lsp/requests/resolve.py
+++ coq/lsp/requests/resolve.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import MutableSequence, Optional
 
 from pynvim import Nvim
 
@@ -10,16 +10,23 @@ from .request import async_request
 
 async def resolve(nvim: Nvim, extern: ExternLSP) -> Optional[Completion]:
     name = "lsp_third_party_resolve" if isinstance(extern, ExternLUA) else "lsp_resolve"
+    comps: MutableSequence[Completion] = []
 
     async for client, resp in async_request(nvim, name, extern.item):
-        if client == extern.client:
-            comp = parse_item(
-                type(extern),
-                client=client,
-                short_name="",
-                weight_adjust=0,
-                item=resp,
-            )
+        comp = parse_item(
+            type(extern),
+            client=client,
+            short_name="",
+            weight_adjust=0,
+            item=resp,
+        )
+        if comp:
+            comps.append(comp)
+        if extern.client and client == extern.client:
             return comp
     else:
-        return None
+        for comp in comps:
+            if comp.doc:
+                return comp
+        else:
+            return None
