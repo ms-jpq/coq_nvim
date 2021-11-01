e2f49d89d17b4a7944f763b867a322a8dd271760
Fly me to the moon <git@bigly.dog>
2021-10-31 22:29:24 -0700

    no assert fasle

--- coq/__main__.py
+++ coq/__main__.py
@@ -138,7 +138,7 @@ elif command == "run":
         lock = ""
     try:
         if not _IN_VENV:
-            assert False
+            raise ImportError()
         elif lock != _REQ:
             raise ImportError()
         else:
