08cb9009df0c65a266b9a976045051819fe15b6f
IIIIII <git@bigly.dog>
2022-08-20 16:44:25 +0000

    less aggressive prefix replacement

--- coq/shared/trans.py
+++ coq/shared/trans.py
@@ -76,7 +76,7 @@ def trans_adjusted(
     )
 
     tokens = len(tuple(coalesce(new_text, unifying_chars=unifying_chars)))
-    old_prefix = simple_before if tokens <= 1 else (edit.old_prefix or simple_before)
+    old_prefix = simple_before if tokens <= 1 else edit.old_prefix
     old_suffix = simple_after if tokens <= 1 else edit.old_suffix
 
     adjusted = ContextualEdit(
