f4ce4141d610cbf9eba95fd69b0be07761a76141
IIIIII <git@bigly.dog>
2023-07-02 14:27:13 -0700

    less restrictive cache

--- coq/shared/repeat.py
+++ coq/shared/repeat.py
@@ -5,13 +5,20 @@ from .types import Edit, RangeEdit, SnippetEdit, SnippetRangeEdit
 
 def sanitize(edit: Edit) -> Optional[Edit]:
     if isinstance(edit, SnippetRangeEdit):
-        if not edit.fallback or edit.fallback == edit.new_text:
+        if (
+            not edit.fallback
+            or edit.begin == edit.end
+            or edit.fallback == edit.new_text
+        ):
             return SnippetEdit(grammar=edit.grammar, new_text=edit.new_text)
         else:
             return Edit(new_text=edit.fallback)
     elif isinstance(edit, SnippetEdit):
         return edit
     elif isinstance(edit, RangeEdit):
-        return None
+        if edit.begin == edit.end:
+            return Edit(new_text=edit.fallback)
+        else:
+            return edit
     else:
         return Edit(new_text=edit.new_text)
