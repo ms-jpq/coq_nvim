dfc7b413b1a85b375b6c94610c7d65b7b974ff46
IIIIII <git@bigly.dog>
2023-07-02 18:17:04 -0700

    fix

--- coq/shared/repeat.py
+++ coq/shared/repeat.py
@@ -1,16 +1,16 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 from ..lsp.types import Cursors
-from .types import Edit, RangeEdit, SnippetEdit, SnippetRangeEdit
+from .types import BaseRangeEdit, Edit, RangeEdit, SnippetEdit, SnippetRangeEdit
+
+
+def _shift(edit: BaseRangeEdit) -> Tuple[int, int, int, int]:
+    return 0, 0, 0, 0
 
 
 def sanitize(cursors: Optional[Cursors], edit: Edit) -> Optional[Edit]:
     if isinstance(edit, SnippetRangeEdit):
-        if (
-            not edit.fallback
-            or edit.begin == edit.end
-            or edit.fallback == edit.new_text
-        ):
+        if not edit.fallback or edit.fallback == edit.new_text:
             return SnippetEdit(grammar=edit.grammar, new_text=edit.new_text)
         else:
             return Edit(new_text=edit.fallback)
@@ -18,8 +18,8 @@ def sanitize(cursors: Optional[Cursors], edit: Edit) -> Optional[Edit]:
         return edit
     elif isinstance(edit, RangeEdit):
         if edit.begin == edit.end:
-            return Edit(new_text=edit.fallback)
-        else:
             return edit
+        else:
+            return Edit(new_text=edit.fallback)
     else:
         return Edit(new_text=edit.new_text)
