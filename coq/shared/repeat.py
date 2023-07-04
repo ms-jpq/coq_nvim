2c9f68bb5d6a1df2f27b4fe10d7fa9214a11d429
IIIIII <git@bigly.dog>
2023-07-04 16:09:27 -0700

    off by 1

--- coq/shared/repeat.py
+++ coq/shared/repeat.py
@@ -37,7 +37,7 @@ def _shift(cursors: Cursors, edit: BaseRangeEdit) -> Tuple[WTF8Pos, WTF8Pos]:
     diff = col - prev_col
 
     if b_row == row:
-        if b_col >= prev_col:
+        if b_col + 1 >= prev_col:
             new_b_col = b_col + diff
         else:
             new_b_col = b_col
@@ -45,7 +45,7 @@ def _shift(cursors: Cursors, edit: BaseRangeEdit) -> Tuple[WTF8Pos, WTF8Pos]:
         new_b_col = b_col
 
     if e_row == row:
-        if e_col >= prev_col:
+        if e_col + 1 >= prev_col:
             new_e_col = e_col + diff
         else:
             new_e_col = e_col
