fccbe74148daafc46521c6b1d5df8173f756c0c9
IIIIII <git@bigly.dog>
2023-07-14 03:47:40 -0700

    reg

--- coq/_registry.py
+++ coq/_registry.py
@@ -3,4 +3,19 @@ from .server.registrants import attachment, autocmds, help, marks, noop, omnifun
 from .server.registrants import preview as rp
 from .server.registrants import repeat, snippets, stats, user_snippets
 
+assert attachment
+assert autocmds
+assert completion
+assert help
+assert marks
+assert noop
+assert omnifunc
+assert repeat
+assert request
+assert resolve
+assert rp
+assert snippets
+assert stats
+assert user_snippets
+
 ____ = None
