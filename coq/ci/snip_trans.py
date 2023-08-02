58bb5f659af540d5a6d28a4a61ef6d55bca217b7
IIIIII <github@bigly.dog>
2023-08-01 22:32:34 -0700

    sub

--- coq/ci/snip_trans.py
+++ coq/ci/snip_trans.py
@@ -1,13 +1,14 @@
 from dataclasses import replace
-
-from std2.string import removesuffix
+from re import compile
 
 from ..snippets.types import ParsedSnippet
+
+_JS = compile(r";\s?")
 
 
 def trans(snip: ParsedSnippet) -> ParsedSnippet:
     if snip.filetype in {"javascript", "typescript", "typescriptreact"}:
-        content = removesuffix(snip.content, suffix=";")
+        content = _JS.sub("", snip.content)
         return replace(snip, content=content)
     else:
         return snip
