a8d41008b11024ad7d3b2fbbb7f5c6995b0b64ee
IIIIII <github@bigly.dog>
2023-08-01 22:46:17 -0700

    pp

--- coq/ci/snip_trans.py
+++ coq/ci/snip_trans.py
@@ -3,12 +3,12 @@ from re import RegexFlag, compile
 
 from ..snippets.types import ParsedSnippet
 
-_JS = compile(r";\s?$", flags=RegexFlag.MULTILINE)
+_JS = compile(r";$|;(\n)?$", flags=RegexFlag.MULTILINE)
 
 
 def trans(snip: ParsedSnippet) -> ParsedSnippet:
     if snip.filetype in {"javascript", "typescript", "typescriptreact"}:
-        content = _JS.sub("", snip.content)
+        content = _JS.sub(r"\1", snip.content)
         return replace(snip, content=content)
     else:
         return snip
