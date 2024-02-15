9c8c9a9d22faf49044086b1c297059cac6406c5e
dogdogdog <github@bigly.dog>
2024-02-15 12:14:10 -0800

    ex

--- coq/shared/executor.py
+++ coq/shared/executor.py
@@ -9,54 +9,14 @@ from asyncio import (
 from concurrent.futures import Future, InvalidStateError
 from contextlib import suppress
 from functools import lru_cache
-from queue import SimpleQueue
 from shutil import which
 from subprocess import CalledProcessError
 from threading import Thread
 from typing import Any, Awaitable, Callable, Sequence, TypeVar, cast
 
-from pynvim_pp.logging import suppress_and_log
 from std2.asyncio.subprocess import call
 
 _T = TypeVar("_T")
-
-
-# class SingleThreadExecutor:
-#     def __init__(self) -> None:
-#         self._q: SimpleQueue = SimpleQueue()
-
-#         def cont() -> None:
-#             while True:
-#                 with suppress_and_log():
-#                     f = self._q.get()
-#                     f()
-
-#         self._th = Thread(daemon=True, target=cont)
-#         self._th.start()
-
-#     def _submit(self, f: Callable[..., Any], *args: Any, **kwargs: Any) -> Future:
-#         fut: Future = Future()
-
-#         def cont() -> None:
-#             try:
-#                 ret = f(*args, **kwargs)
-#             except BaseException as e:
-#                 with suppress(InvalidStateError):
-#                     fut.set_exception(e)
-#             else:
-#                 with suppress(InvalidStateError):
-#                     fut.set_result(ret)
-
-#         self._q.put(cont)
-#         return fut
-
-#     def ssubmit(self, f: Callable[..., _T], *args: Any, **kwargs: Any) -> _T:
-#         fut = self._submit(f, *args, **kwargs)
-#         result = cast(_T, fut.result())
-#         return result
-
-#     def submit(self, f: Callable[..., _T], *args: Any, **kwargs: Any) -> Awaitable[_T]:
-#         return wrap_future(self._submit(f, *args, **kwargs))
 
 
 class AsyncExecutor:
@@ -79,14 +39,15 @@ class AsyncExecutor:
         fut: Future = Future()
 
         def cont() -> None:
-            try:
-                ret = f(*args, **kwargs)
-            except BaseException as e:
-                with suppress(InvalidStateError):
-                    fut.set_exception(e)
-            else:
-                with suppress(InvalidStateError):
-                    fut.set_result(ret)
+            if fut.set_running_or_notify_cancel():
+                try:
+                    ret = f(*args, **kwargs)
+                except BaseException as e:
+                    with suppress(InvalidStateError):
+                        fut.set_exception(e)
+                else:
+                    with suppress(InvalidStateError):
+                        fut.set_result(ret)
 
         self._loop.call_soon_threadsafe(cont)
         return fut
