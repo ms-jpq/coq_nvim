3e12ac6c0f2be63d5e7eb7fde48dc356463b042c
dogisgreat <git@bigly.dog>
2025-05-30 11:00:11 -0400

    allow tabby agent

--- coq/clients/lsp/worker.py
+++ coq/clients/lsp/worker.py
@@ -109,7 +109,7 @@ class Worker(BaseWorker[LSPClient, None]):
             weight_adjust=self._options.weight_adjust,
             context=context,
             chunk=self._max_results,
-            clients={"tabby_ml"},
+            clients=set(),
         )
         async for row, peers, elapsed in rows:
             self._stats.update(peers, client=row.client, elapsed=elapsed)
