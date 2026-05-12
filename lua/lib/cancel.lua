4e6fd40ed75ce9a9ea22f668862715c68a52d8f8
dogisgreat <git@bigly.dog>
2026-05-12 19:38:30 -0400

    add cancel

---
+++ lua/lib/cancel.lua
@@ -0,0 +1,41 @@
+return {
+  token = function(parent)
+    local watchers = {}
+    local token = { cancelled = false }
+
+    local function fire(watcher)
+      if type(watcher) == "function" then
+        watcher()
+      else
+        watcher.cancel()
+      end
+    end
+
+    token.cancel = function()
+      if token.cancelled then
+        return
+      end
+      token.cancelled = true
+      for _, watcher in pairs(watchers) do
+        fire(watcher)
+      end
+    end
+
+    token.watch = function(watcher)
+      if token.cancelled then
+        fire(watcher)
+      else
+        table.insert(watchers, watcher)
+      end
+    end
+
+    if parent then
+      parent.watch(token)
+      if parent.cancelled then
+        token.cancel()
+      end
+    end
+
+    return token
+  end,
+}
