b3b6efefaaae66152fa02610f430f8c97dfb732a
dogisgreat <git@bigly.dog>
2026-05-12 18:45:52 -0400

    add test runner

---
+++ lua/lib/test.lua
@@ -0,0 +1,30 @@
+local registry = {}
+
+return {
+  test = function(name, fn)
+    table.insert(registry, { name = name, fn = fn })
+  end,
+  eq = function(a, b)
+    if not vim.deep_equal(a, b) then
+      error(("eq failed:\n  lhs = %s\n  rhs = %s"):format(vim.inspect(a), vim.inspect(b)), 2)
+    end
+  end,
+  run = function()
+    local failed = 0
+    for _, t in ipairs(registry) do
+      local ok, err = xpcall(t.fn, debug.traceback)
+
+      if ok then
+        vim.notify("  ✓ " .. t.name, vim.log.levels.INFO)
+      else
+        vim.notify("  ✗ " .. t.name .. "\n" .. err, vim.log.levels.ERROR)
+        failed = failed + 1
+      end
+    end
+
+    if failed > 0 then
+      vim.notify(("%d failed"):format(failed), vim.log.levels.ERROR)
+      os.exit(1)
+    end
+  end,
+}
--- test.lua
+++ test.lua
@@ -1,6 +1,10 @@
 #!/usr/bin/env -S -- nvim -l
 
+local T = require "lib.test"
+
 for _, f in ipairs(vim.fn.globpath("lua", "**/*.test.lua", false, true)) do
   vim.notify([[🧪 ]] .. f, vim.log.levels.INFO)
   vim.cmd("source " .. vim.fn.fnameescape(f))
 end
+
+T.run()
